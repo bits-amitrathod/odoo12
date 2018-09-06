7463b142943d2ebf6c674b706c34aa483b971287
avadhoot12 <avadhootkharpe@benchmarkitsolutions.com>
2018-09-06 20:39:00 +0530

    product expiry module name changed.

--- product_expiry_extension/__manifest__.py
+++ product_expiry_extension/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "product_expiry_extension",
+    'name': "Product Expiry Extension",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
