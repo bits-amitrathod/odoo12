42e80aabe18919ad30fbb5e9d2c4860009b45e15
anvay <anvaywalujkar@benchmarkitsolutions.com>
2018-09-11 20:09:23 +0530

    Vendor offer changes sales price

--- vendor_offer/__manifest__.py
+++ vendor_offer/__manifest__.py
@@ -10,8 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Benchmark It Solutions",
+    'website': "http://www.benchmarkitsolutions.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
@@ -27,11 +27,9 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'demo/data.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+
     'application' :True,
     'auto-install': True,
     'installable': True,
