bf45827b1cb643598e61389a2a714790b31085be
amit rathod <amit@localhost.localdomain>
2018-09-18 19:00:59 +0530

    engine to sent status change

--- prioritization_engine/models/saleorder.py
+++ prioritization_engine/models/saleorder.py
@@ -58,6 +58,43 @@ class SaleOrder(models.Model):
         if len(multi) >= 1:
             return multi.do_unreserve()
 
+    @api.multi
+    def action_quotation_send(self):
+        """
+        This function opens a window to compose an email, with the edi sale template message loaded by default
+        """
+        self.ensure_one()
+        ir_model_data = self.env['ir.model.data']
+        try:
+            template_id = ir_model_data.get_object_reference('sale', 'email_template_edi_sale')[1]
+        except ValueError:
+            template_id = False
+        try:
+            compose_form_id = ir_model_data.get_object_reference('mail', 'email_compose_message_wizard_form')[1]
+        except ValueError:
+            compose_form_id = False
+        ctx = {
+            'default_model': 'sale.order',
+            'default_res_id': self.ids[0],
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'default_composition_mode': 'comment',
+            'mark_so_as_sent': True,
+            'custom_layout': "sale.mail_template_data_notification_email_sale_order",
+            'proforma': self.env.context.get('proforma', False),
+            'force_email': True
+        }
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(compose_form_id, 'form')],
+            'view_id': compose_form_id,
+            'target': 'new',
+            'context': ctx,
+        }
+
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
