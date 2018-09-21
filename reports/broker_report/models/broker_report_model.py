67826508799fa0fc65f0a8180560157cd4b025cb
anvay <anvaywalujkar@benchmarkitsolutions.com>
2018-09-21 17:51:50 +0530

    reports changes

--- reports/broker_report/models/broker_report_model.py
+++ reports/broker_report/models/broker_report_model.py
@@ -6,7 +6,7 @@ from odoo import models, fields, api
 class broker_report(models.Model):
     _inherit = 'purchase.order'
 
-    total_retail_broker = fields.Monetary('Total Retail', store=False, compute='_compute_all_vals')
+    total_retail_broker = fields.Monetary('Total Retail', store=False)
     bonus_eligible = fields.Monetary(string='Bonus Eligible', store=False)
     hospital_total = fields.Monetary(string='Hospital Total',store=False)
     broker_total = fields.Monetary('Broker Total', store=False)
@@ -31,57 +31,57 @@ class broker_report(models.Model):
     broker_desc_mar = fields.Char('Desc', store=False)
 
 
-    @api.multi
-    def _compute_all_vals(self):
-        apprisal_list=[]
-        for order in self:
-            apprisal_list.append(order)
-        apprisal_list_rtl_val = apprisal_list_tot_val = apprisal_list_mar_val = apprisal_list[0]
-        apprisal_list_report = []
-        tot_offer = 0
-        margin40tot = 0
-        for order in self:
-            apprisal_list_rtl_val.total_retail_broker = apprisal_list_rtl_val.total_retail_broker + order.retail_amt
-            apprisal_list_rtl_val.bonus_eligible = apprisal_list_rtl_val.bonus_eligible + order.bonus_eligible
-            apprisal_list_rtl_val.hospital_total = apprisal_list_rtl_val.hospital_total + order.hospital_total
-            apprisal_list_rtl_val.broker_total = apprisal_list_rtl_val.broker_total + order.broker_total
-            apprisal_list_rtl_val.broker_greater_40_total = apprisal_list_rtl_val.broker_greater_40_total + order.broker_greater_40_total
-            apprisal_list_rtl_val.broker_less_40_total = apprisal_list_rtl_val.broker_less_40_total + order.broker_less_40_total
-            apprisal_list_rtl_val.broker_desc = "RETAIL $ VALUE"
-            tot_offer = tot_offer + order.offer_amount
-            if (order.retail_amt != 0):
-                if (abs(float(((order.offer_amount) / float(order.retail_amt)) - 1)) < 0.4):
-                    margin40tot = float(margin40tot) + order.retail_amt
-
-        apprisal_list_rtl_val.bonus_eligible = apprisal_list_rtl_val.total_retail_broker - margin40tot
-
-        apprisal_list_tot_val.total_retail_broker_tot = str(round(float(
-            apprisal_list_rtl_val.total_retail_broker / apprisal_list_rtl_val.total_retail_broker) * 100,
-                                                                  2))
-        apprisal_list_tot_val.bonus_eligible_tot = str(
-            round((apprisal_list_rtl_val.bonus_eligible / apprisal_list_rtl_val.total_retail_broker) * 100,
-                  2))
-        apprisal_list_tot_val.hospital_total_tot = apprisal_list_rtl_val.hospital_total + 1
-        apprisal_list_tot_val.broker_total_tot = apprisal_list_rtl_val.broker_total + 1
-        apprisal_list_tot_val.broker_greater_40_total_tot = apprisal_list_rtl_val.broker_greater_40_total + 1
-        apprisal_list_tot_val.broker_less_40_total_tot = apprisal_list_rtl_val.broker_less_40_total + 1
-        apprisal_list_rtl_val.broker_desc_tot = "% OF TOTAL"
-
-        apprisal_list_mar_val.total_retail_broker_mar = str(
-            round(abs(1 - float(float(tot_offer) / apprisal_list_rtl_val.total_retail_broker)) * 100,
-                  2)) + ' %'
-        apprisal_list_mar_val.bonus_eligible_mar = str(round(abs(1 - float(
-            float(apprisal_list_tot_val.bonus_eligible_tot) / float(
-                apprisal_list_tot_val.bonus_eligible_tot))) * 100, 2)) + ' %'
-        apprisal_list_mar_val.hospital_total_mar = apprisal_list_rtl_val.hospital_total + 2
-        apprisal_list_mar_val.broker_total_mar = apprisal_list_rtl_val.broker_total + 2
-        apprisal_list_mar_val.broker_greater_40_total_mar = apprisal_list_rtl_val.broker_greater_40_total + 2
-        apprisal_list_mar_val.broker_less_40_total_mar = apprisal_list_rtl_val.broker_less_40_total + 2
-        apprisal_list_rtl_val.broker_desc_mar = "MARGIN %"
-
-        apprisal_list_report.append(apprisal_list_rtl_val)
-        apprisal_list_report.append(apprisal_list_tot_val)
-        apprisal_list_report.append(apprisal_list_mar_val)
+    # @api.multi
+    # def _compute_all_vals(self):
+    #     apprisal_list=[]
+    #     for order in self:
+    #         apprisal_list.append(order)
+    #     apprisal_list_rtl_val = apprisal_list_tot_val = apprisal_list_mar_val = apprisal_list[0]
+    #     apprisal_list_report = []
+    #     tot_offer = 0
+    #     margin40tot = 0
+    #     for order in self:
+    #         apprisal_list_rtl_val.total_retail_broker = apprisal_list_rtl_val.total_retail_broker + order.retail_amt
+    #         apprisal_list_rtl_val.bonus_eligible = apprisal_list_rtl_val.bonus_eligible + order.bonus_eligible
+    #         apprisal_list_rtl_val.hospital_total = apprisal_list_rtl_val.hospital_total + order.hospital_total
+    #         apprisal_list_rtl_val.broker_total = apprisal_list_rtl_val.broker_total + order.broker_total
+    #         apprisal_list_rtl_val.broker_greater_40_total = apprisal_list_rtl_val.broker_greater_40_total + order.broker_greater_40_total
+    #         apprisal_list_rtl_val.broker_less_40_total = apprisal_list_rtl_val.broker_less_40_total + order.broker_less_40_total
+    #         apprisal_list_rtl_val.broker_desc = "RETAIL $ VALUE"
+    #         tot_offer = tot_offer + order.offer_amount
+    #         if (order.retail_amt != 0):
+    #             if (abs(float(((order.offer_amount) / float(order.retail_amt)) - 1)) < 0.4):
+    #                 margin40tot = float(margin40tot) + order.retail_amt
+    #
+    #     apprisal_list_rtl_val.bonus_eligible = apprisal_list_rtl_val.total_retail_broker - margin40tot
+    #
+    #     apprisal_list_tot_val.total_retail_broker_tot = str(round(float(
+    #         apprisal_list_rtl_val.total_retail_broker / apprisal_list_rtl_val.total_retail_broker) * 100,
+    #                                                               2))
+    #     apprisal_list_tot_val.bonus_eligible_tot = str(
+    #         round((apprisal_list_rtl_val.bonus_eligible / apprisal_list_rtl_val.total_retail_broker) * 100,
+    #               2))
+    #     apprisal_list_tot_val.hospital_total_tot = apprisal_list_rtl_val.hospital_total + 1
+    #     apprisal_list_tot_val.broker_total_tot = apprisal_list_rtl_val.broker_total + 1
+    #     apprisal_list_tot_val.broker_greater_40_total_tot = apprisal_list_rtl_val.broker_greater_40_total + 1
+    #     apprisal_list_tot_val.broker_less_40_total_tot = apprisal_list_rtl_val.broker_less_40_total + 1
+    #     apprisal_list_rtl_val.broker_desc_tot = "% OF TOTAL"
+    #
+    #     apprisal_list_mar_val.total_retail_broker_mar = str(
+    #         round(abs(1 - float(float(tot_offer) / apprisal_list_rtl_val.total_retail_broker)) * 100,
+    #               2)) + ' %'
+    #     apprisal_list_mar_val.bonus_eligible_mar = str(round(abs(1 - float(
+    #         float(apprisal_list_tot_val.bonus_eligible_tot) / float(
+    #             apprisal_list_tot_val.bonus_eligible_tot))) * 100, 2)) + ' %'
+    #     apprisal_list_mar_val.hospital_total_mar = apprisal_list_rtl_val.hospital_total + 2
+    #     apprisal_list_mar_val.broker_total_mar = apprisal_list_rtl_val.broker_total + 2
+    #     apprisal_list_mar_val.broker_greater_40_total_mar = apprisal_list_rtl_val.broker_greater_40_total + 2
+    #     apprisal_list_mar_val.broker_less_40_total_mar = apprisal_list_rtl_val.broker_less_40_total + 2
+    #     apprisal_list_rtl_val.broker_desc_mar = "MARGIN %"
+    #
+    #     apprisal_list_report.append(apprisal_list_rtl_val)
+    #     apprisal_list_report.append(apprisal_list_tot_val)
+    #     apprisal_list_report.append(apprisal_list_mar_val)
 
 
 
--- reports/broker_report/models/popup_view_model.py
+++ reports/broker_report/models/popup_view_model.py
@@ -19,23 +19,26 @@ class BrokerReportPopUp(models.TransientModel):
 
         tree_view_id = self.env.ref('broker_report.broker_form_list').id
         form_view_id = self.env.ref('apprisal_tracker.apprisal_tracker_offer_form').id
-        if self.compute_at_date:
-            action = {
-                'type': 'ir.actions.act_window',
-                'views': [(tree_view_id, 'tree'), (form_view_id, 'form')],
-                'view_mode': 'tree,form',
-                'name': _('Product Vendor'),
-                'res_model': 'purchase.order',
-                'domain': [('confirmation_date','>=', self.start_date ),('confirmation_date','<=', self.end_date ),('state','in',('purchase','done'))],
-            }
-            return action
-        else:
-            action = {
-                'type': 'ir.actions.act_window',
-                'views': [(tree_view_id, 'tree'), (form_view_id, 'form')],
-                'view_mode': 'tree,form',
-                'name': _('Product Vendor'),
-                'res_model': 'purchase.order',
-                'domain': [('state', 'in', ('purchase', 'done'))],
-            }
-            return action
+
+        # if self.compute_at_date:
+        #     action = {
+        #         'type': 'ir.actions.act_window',
+        #         'views': [(tree_view_id, 'tree'), (form_view_id, 'form')],
+        #         'view_mode': 'tree,form',
+        #         'name': _('Product Vendor'),
+        #         'res_model': 'purchase.order',
+        #         'domain': [('confirmation_date','>=', self.start_date ),('confirmation_date','<=', self.end_date ),('state','in',('purchase','done'))],
+        #     }
+        #     return action
+        # else:
+        #     action = {
+        #         'type': 'ir.actions.act_window',
+        #         'views': [(tree_view_id, 'tree'), (form_view_id, 'form')],
+        #         'view_mode': 'tree,form',
+        #         'name': _('Product Vendor'),
+        #         'res_model': 'purchase.order',
+        #         'domain': [('state', 'in', ('purchase', 'done'))],
+        #     }
+        #     return action
+
+        return self.env.ref('broker_report.action_report_broker_report').report_action([], data={})
