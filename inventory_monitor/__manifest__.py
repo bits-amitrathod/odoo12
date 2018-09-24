072e321cac1f415a08e6bbfec0055beca53e8a59
avadhoot12 <avadhootkharpe@benchmarkitsolutions.com>
2018-09-24 12:52:41 +0530

    inventory_monitor module added

---
+++ inventory_monitor/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Inventory Monitor",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Benchmarkitsolutions",
+    'website': "http://www.benchmarkitsolutions.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','stock','product_brand','prioritization_engine','inventory_extension'],
+    # always loaded
+
+
+
+    'data': [
+        'views/inventory_monitor.xml',
+        'views/inventory_monitor_print.xml',
+        'views/inventory_monitor_print_report.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
