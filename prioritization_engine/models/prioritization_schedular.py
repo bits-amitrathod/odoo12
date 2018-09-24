dc6789ed88abe9f6e23c2adf1a2869278852b31b
amolshirude@benchmarkitsolutions.com <amolshirude@benchmarkitsolutions.com>
2018-09-24 16:52:25 +0530

    working on length of hold

--- prioritization_engine/models/prioritization_schedular.py
+++ prioritization_engine/models/prioritization_schedular.py
@@ -2,6 +2,7 @@
 
 from odoo import models, fields, api
 import logging
+from datetime import datetime
 
 _logger = logging.getLogger(__name__)
 
@@ -13,7 +14,34 @@ class PrioritizationScheduler(models.Model):
     @api.multi
     def process_prioritization_scheduler(self):
         _logger.info('In PrioritizationScheduler')
+        self.check_length_of_hold()
         self.env['sps.customer.requests'].get_customer_requests()
+
+    def check_length_of_hold(self):
+        _logger.info('In check_length_of_hold')
+
+        sale_order_list = self.env['sale.order'].search([('state', 'in', ('sent','engine')), ('team_id.team_type', '=', 'engine')])
+
+        for sale_order in sale_order_list:
+            _logger.info('sale_order : %r', sale_order.id)
+            sale_order_line_list = self.env['sale.order.line'].search([('order_id', '=', sale_order.id)])
+            for sale_order_line in sale_order_line_list:
+                _logger.info('sale_order_line : %r : %r',sale_order_line.id, sale_order_line.product_id.id)
+
+            # length_of_hold = 0
+            # sale_order['create_date'] + length_of_hold
+            #
+            # # get current datetime
+            # current_datetime = datetime.now()
+
+
+
+
+
+
+
+
+
 
 
 
