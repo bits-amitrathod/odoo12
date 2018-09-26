dfe74bab918b3285dfc89d6dd5f1ca74f44fd344
anvay <anvaywalujkar@benchmarkitsolutions.com>
2018-09-26 16:41:12 +0530

    appraisal tool name

--- apprisal_tracker/__manifest__.py
+++ apprisal_tracker/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "apprisal_tracker",
+    'name': "Appraisal Tracker",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
