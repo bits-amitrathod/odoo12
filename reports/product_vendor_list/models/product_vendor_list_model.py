4c67b486db825ee72f98b05f25e125ea8d75c712
anvay <anvaywalujkar@benchmarkitsolutions.com>
2018-09-28 17:18:01 +0530

    SKu name changes

--- reports/product_vendor_list/models/product_vendor_list_model.py
+++ reports/product_vendor_list/models/product_vendor_list_model.py
@@ -3,11 +3,14 @@ from odoo import api, fields, models
 
 
 class ProductVendorListView(models.Model):
+
+    # _name='productvendorlist.productvendorlist'
     _inherit = 'purchase.order'
+    # _auto=False
 
     last_sold = fields.Date('Last Sold',  store=False)
     cost=fields.Monetary(string='Cost', store=False)
-    sku_code_product = fields.Char(string='Product Number', compute='_compute_product_vals', store=False)
+    sku_code_product = fields.Char(string='SKU/Catelog No.', compute='_compute_product_vals', store=False)
 
     @api.onchange('cost')
     def _compute_product_vals(self):
@@ -17,7 +20,7 @@ class ProductVendorListView(models.Model):
             order.sku_code_product=product.sku_code
             order.cost=product.list_price
             # order = "confirmation_date desc", limit = 1
-            sale_orders = self.env['sale.order'].search([('product_id', '=', order.product_id.id), ('state', '=', 'sale')])
+            sale_orders = self.env['sale.order'].search([('product_id', '=', order.product_id.id), ('state', '=', 'sale')],order = "confirmation_date desc", limit = 1)
             for order_temp in sale_orders:
                 order.last_sold=fields.Datetime.from_string(order_temp.confirmation_date).date()
 
@@ -27,3 +30,4 @@ class ProductVendorListView(models.Model):
             # query_result = order.env.cr.dictfetchone()
             # if query_result['max'] != None:
             #     order.last_sold = fields.Datetime.from_string(str(query_result['max'])).date()
+
