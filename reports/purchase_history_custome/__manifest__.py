06fa76d44ecbca8867dbc33afe1ab5a9ae450587
akashingole <akashingole@benchmarkitsolutions.com>
2018-10-03 16:59:24 +0530

    reports UI changes

--- reports/purchase_history_custome/__manifest__.py
+++ reports/purchase_history_custome/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Purchase History Custom",
+    'name': "Purchase History",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
