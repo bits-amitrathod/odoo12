ea5d805c8b8bd88b82ed0119e2287d4b895c91fd
Manohar <manoharbomma@benchmarkitsolutions.com>
2018-10-08 11:27:37 +0530

    completed returned sales report

--- reports/returned_sales/report/returned_sales_report_model.py
+++ reports/returned_sales/report/returned_sales_report_model.py
@@ -5,29 +5,37 @@ log = logging.getLogger(__name__)
 
 
 class ReportProductsOnOrder(models.AbstractModel):
-    _name = 'report.returned_sales.prod_on_order_temp'
+    _name = 'report.returned_sales.returned_sales_temp'
 
     @api.model
     def get_report_values(self, docids, data=None):
-        returned_sales_list = self.env['products.on_order'].browse(docids)
+        returned_sales_list = self.env['returned_sales.order'].browse(docids)
 
         group_by_list = {}
         for returned_sales in returned_sales_list:
-            order_line = [returned_sales.product_id.product_tmpl_id.name, returned_sales.partner_id.display_name,
-                          returned_sales.qty_ordered, returned_sales.qty_remaining]
+            order_line = [returned_sales.partner_id.display_name,
+                          returned_sales.done_qty, returned_sales.cost_price]
+            key = ""
+            if returned_sales.product_id.product_tmpl_id.sku_code:
+                key = str(returned_sales.product_id.product_tmpl_id.sku_code) + str(" - ")
+
+            key = key + str(returned_sales.product_id.product_tmpl_id.name)
             try:
-                group_by_list[returned_sales.name].append(order_line)
+                group_by_list[key].append(order_line)
             except KeyError:
-                group_by_list[returned_sales.name] = [order_line]
+                group_by_list[key] = [order_line]
 
 
         final_list = []
-        for order_name, line_list in group_by_list.items():
-            inner_list = [order_name, line_list]
-            remainig_so_qty = 0
+        for product_name, line_list in group_by_list.items():
+            inner_list = [product_name, line_list]
+            total_done_qty = 0.0
+            total_cost_price = 0.00
             for line in line_list:
-                remainig_so_qty = remainig_so_qty + line[3]
-            inner_list.append(remainig_so_qty)
+                total_done_qty = total_done_qty + line[1]
+                total_cost_price = total_cost_price + line[2]
+            inner_list.append(total_done_qty)
+            inner_list.append(total_cost_price)
             final_list.append(inner_list)
 
         log.info('final list: %r', final_list)
