ecb8efafd8ece483140bafdb164a6f4e9d14262c
Manohar <manoharbomma@benchmarkitsolutions.com>
2018-10-09 11:01:22 +0530

    on_hand_by_date  reports changes

--- reports/on_hand_by_date/models/on_hand_by_date_model.py
+++ reports/on_hand_by_date/models/on_hand_by_date_model.py
@@ -19,6 +19,7 @@ class OnHandByDate(models.Model):
     partner_id = fields.Many2one('res.partner', string='Vendor',)
     sku_code = fields.Char()
     vendor_name = fields.Char()
+    report_date = fields.Char()
 
     @api.model_cr
     def init(self):
@@ -29,28 +30,38 @@ class OnHandByDate(models.Model):
         report_date = self.env.context.get('report_date')
         if report_date is None:
             report_date = datetime.now().strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+
+            include_zero_quantities = self.env.context.get('include_zero_quantities')
+
+        if not include_zero_quantities is None and include_zero_quantities:
+            include_zero_quantities = " FULL JOIN "
+        else:
+            include_zero_quantities = " INNER JOIN "
+
         sql_query = """  CREATE VIEW on_hand_by_date_stock AS ( 
-                    
- SELECT  
-        ROW_NUMBER () OVER (ORDER BY product_id) as id, p.id as product_id, 
-         q.qty_on_hand, b.name as vendor_name, t.name as name, 
-         t.sku_code as sku_code, b.partner_id as partner_id
- FROM (
-        SELECT 
-                sq.product_id as product_id, SUM(sq.quantity) as qty_on_hand
-        FROM stock_quant sq LEFT JOIN stock_production_lot l ON sq.lot_id = l.id LEFT JOIN stock_location lc ON lc.id = sq.location_id 
+        SELECT  
+            ROW_NUMBER () OVER (ORDER BY product_id) as id, p.id as product_id, 
+            q.qty_on_hand, b.name as vendor_name, t.name as name, 
+            t.sku_code as sku_code, b.partner_id as partner_id, """ + str(report_date) + """ as report_date
+        FROM (
+                SELECT 
+                    sq.product_id as product_id, SUM(sq.quantity) as qty_on_hand
+                FROM 
+                    stock_quant sq LEFT JOIN stock_production_lot l ON sq.lot_id = l.id LEFT JOIN stock_location lc 
+                ON 
+                    lc.id = sq.location_id 
+                WHERE 
+                    (lc.create_date <= '""" + str(report_date) + """' AND l.create_date <= '""" + str(report_date) + """') 
+                    AND 
+                    sq.quantity >= 0  GROUP BY (sq.product_id)
+            ) q """ + include_zero_quantities + """ 
+            product_product p ON p.id = q.product_id
+        LEFT JOIN 
+            product_template t ON t.id = p.product_tmpl_id
+        LEFT JOIN 
+            product_brand b ON b.id = t.product_brand_id                      
         WHERE 
-                (lc.create_date <= '""" + str(report_date) + """' OR l.create_date <= '""" + str(report_date) + """') 
-                AND sq.quantity >= 0  GROUP BY (sq.product_id)
-       ) q 
- FULL JOIN 
-      product_product p ON p.id = q.product_id
- LEFT JOIN 
-      product_template t ON t.id = p.product_tmpl_id
- LEFT JOIN 
-      product_brand b ON b.id = t.product_brand_id                      
- WHERE 
-      p.create_date <= '""" + str(report_date) + "' "
+            p.create_date <= '""" + str(report_date) + "' "
 
         partner_id = self.env.context.get('partner_id')
         product_id = self.env.context.get('product_id')
