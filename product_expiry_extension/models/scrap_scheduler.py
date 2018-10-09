21f3eda284980ccf9ebddff7a6b1005c28a6fbea
avadhoot12 <avadhootkharpe@benchmarkitsolutions.com>
2018-10-09 17:10:12 +0530

    scrap schedular change to Transient.

--- product_expiry_extension/models/scrap_scheduler.py
+++ product_expiry_extension/models/scrap_scheduler.py
@@ -9,9 +9,9 @@ from odoo.tools import float_compare
 
 _logger = logging.getLogger(__name__)
 
-class ScrapScheduler(models.Model):
-    _inherit = 'stock.scrap'
+class ScrapScheduler(models.TransientModel):
     _name = 'stock.scrap.scheduler'
+
 
     @api.model
     @api.multi
@@ -34,18 +34,19 @@ class ScrapScheduler(models.Model):
             for location_id in location_ids:
                 stock_ids=self.env['stock.quant'].search([('lot_id','=',stock_product_lot.id),('location_id', '=', location_id.id),('quantity', '>', 0)])
                 for stock in stock_ids:
+                    _logger.info("stock.product_id : %r",stock.product_id )
                     scrap_id=self.env['stock.location'].search([('name', '=', 'Scrapped'), ('active', '=', True)]).id
                     val = {'location_id': location_id.id, 'date_expected': today_start, 'scrap_qty': int(stock.quantity),
                         'state': 'draft', 'product_id': int(stock.product_id), 'scrap_location_id': scrap_id, 'owner_id': False,
                         'product_uom_id': int(stock_product_lot.product_uom_id), 'package_id': False, 'picking_id': False, 'origin': False,
                         'lot_id': int(stock.lot_id)}
-                    self=self.create(val)
-                    self.action_validate()
+                    ml=self.env['stock.production.lot'].create(val)
+                    ml.action_validate()
 
 
     def process_manual_scrap_scheduler(self):
         _logger.info("process_manual_scrap_scheduler called..")
-        return self.process_scrap_scheduler()
+        self.process_scrap_scheduler()
 
 
 
