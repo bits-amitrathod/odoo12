d9fe0d0a03064075e397f5e88c6bbf376f4e78c0
Manohar <manoharbomma@benchmarkitsolutions.com>
2018-10-16 11:41:03 +0530

    addded mtd_sales report and resolved issues in on_hand reports

--- reports/on_hand_by_expiry/report/on_hand_by_expiration_report_models.py
+++ reports/on_hand_by_expiry/report/on_hand_by_expiration_report_models.py
@@ -1,4 +1,6 @@
 from odoo import api, models
+import datetime
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT, pycompat, misc
 
 
 class OnHandByDateReportModel(models.AbstractModel):
@@ -8,8 +10,12 @@ class OnHandByDateReportModel(models.AbstractModel):
     def get_report_values(self, docids, data=None):
         on_hand_by_expiration_date_stock_list = self.env['on_hand_by_expiry'].browse(docids)
 
+        report_date = datetime.datetime.now().strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+
+        data_dict = dict(items=on_hand_by_expiration_date_stock_list,report_date=report_date)
+
         action = self.env.ref('on_hand_by_expiry.action_report_on_hand_by_expiry').report_action([],
-                                            data=on_hand_by_expiration_date_stock_list)
+                                            data=data_dict)
         action.update({'target': 'main'})
 
         return action
