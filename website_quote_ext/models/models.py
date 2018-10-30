29889eaf6072f011a961fc984ba558d3f749d6fd
amit <amitrathod@benchmarkitsolutions.com>
2018-10-30 16:10:15 +0530

    change in accept

--- website_quote_ext/models/models.py
+++ website_quote_ext/models/models.py
@@ -10,31 +10,11 @@ class Website(models.Model):
 
     @api.multi
     def sale_get_engine_order(self, order_id,line_id,set_qty,product_id):
-        _logger.info("inside sale_get_engine_order")
+
         order = self.env['sale.order'].search([('id', '=', order_id)])[0]
         values = {'product_uom_qty':set_qty}
-        _logger.info(line_id)
         line = self.env['sale.order.line'].sudo().search([('id', '=', line_id)])[0]
         line.write(values)
-        moves = self.env['stock.move'].search([('sale_line_id', '=', line_id)])
-        _logger.info(len(moves))
-        _logger.info(moves)
-        for movelineid in moves:
-            _logger.info("Inside First For")
-            _logger.info(movelineid.move_line_ids)
-            moveline = self.env['stock.move.line'].search([('id', '=', movelineid.move_line_ids)])
-            _logger.info(moveline.package_id)
-            _logger.info(moveline.result_package_id)
-            package=self.env['stock.quant.package'].search([('id', '=', moveline.package_id)])
-            _logger.info(package.quant_ids)
-            quants=self.env['stock.quant'].search([('id', '=', package.quant_ids)])
-            for quant in quants:
-                _logger.info("Inside second For")
-                _logger.info(quant)
-                value = {'quantity':quant.quantity-1,'reserved_quantity': set_qty}
-                quant.write(value)
-                _logger.info(quant)
-        #order.write(line)
         count = self.env['prioritization.engine.model'].get_available_product_count(order.partner_id.id, product_id)
         return count;
 
