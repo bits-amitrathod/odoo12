ad3f1bd3f64389ec92dcb35d3996605881a18090
amit <amitrathod@benchmarkitsolutions.com>
2018-10-30 12:05:33 +0530

    move line Entry

--- website_quote_ext/models/models.py
+++ website_quote_ext/models/models.py
@@ -7,12 +7,14 @@ class Website(models.Model):
 
     @api.multi
     def sale_get_engine_order(self, order_id,line_id,set_qty,product_id):
-        print(order_id)
         print(self.env['sale.order'].search([('id', '=', order_id)]))
         order = self.env['sale.order'].search([('id', '=', order_id)])[0]
         values = {'product_uom_qty':set_qty}
         line = self.env['sale.order.line'].sudo().search([('id', '=', line_id)])[0]
         line.write(values)
+        move = self.env['stock.move'].search([('sale_line_id', '=', line_id)])
+        moveline = self.env['stock.move.line'].search([('move_id', '=', move.id)])
+        moveline.write(values)
         #order.write(line)
         count = self.env['prioritization.engine.model'].get_available_product_count(order.partner_id.id, product_id)
         return count;
