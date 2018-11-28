92ddad15a6035b0af5ed137837b4946368d6a9f4
Ajinkya Nimbalkar <ajinkyanimbalkar@benchmarkitsolutions.com>
2018-11-28 17:33:34 +0530

    Product Activity Report

---
+++ reports/product_activity_report/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': "Product Activity Report",
+
+    'summary': """
+      Product Activity Report
+      """,
+
+    'author': "Benchmark IT Solutions (I) Pvt Ltd.",
+    'website': "http://www.benchmarkitsolutions.com",
+
+    'category': 'Report',
+    'version': '0.1',
+
+    'depends': ['stock_account','prioritization_engine'],
+
+    'data': [
+        'views/views.xml',
+        'report/report_product_activity_report.xml',
+        'report/product_activity_report.xml'
+    ],
+    'auto_install': False,
+    'installable': True,
+    'application': True,
+}
