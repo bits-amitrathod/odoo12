7eff85e797a4e106308a41ced8e5d1bde04128d6
Javed Shaikh <JavedShaikh@BenchmarkItSolutions.Com>
2019-01-02 15:33:26 +0530

    sales purchase history updated.

--- reports/sales_purchase_history_report/models/sale_purchase_history_model.py
+++ reports/sales_purchase_history_report/models/sale_purchase_history_model.py
@@ -11,34 +11,27 @@ _logger = logging.getLogger(__name__)
 class SalePurchaseHistory(models.Model):
     _inherit = "sale.order.line"
 
-    product_name = fields.Char("Product Name", store=False)
-    product_sku = fields.Char("Product SKU", compute='_compare_data', store=False)
+    # product_name = fields.Char("Product Name", compute='_compare_data',store=False)
+    product_sku_ref = fields.Char("Product SKU", compute='_compare_data', store=False)
     customer_name = fields.Char("Customer Name", store=False)
-    sale_order = fields.Char("Order", store=False)
-    delivered_date = fields.Date("Delivered Date", store=False)
-    qty_delivered = fields.Float("Delivered Qty", store=False)
-    unit_price = fields.Monetary("Unit Price", currency_field='currency_id', store=False)
-    total_price = fields.Monetary("Total", currency_field='currency_id', store=False)
-    user_id = fields.Many2one('res.users', string='User', store=False)
-    currency_id = fields.Many2one("res.currency", string="Currency",readonly=True)
-    product_uom = fields.Char(string='UOM', store=False)
+    delivered_date = fields.Date("Delivered Date",compute='_compare_data', store=False)
+    # qty_delivered = fields.Float("Delivered Qty", store=False)
+    # unit_price = fields.Monetary("Unit Price", currency_field='currency_id', store=False)
+    # total_price = fields.Monetary("Total", currency_field='currency_id', store=False)
+    # user_id = fields.Many2one('res.users', string='User', store=False)
+    # currency_id = fields.Many2one("res.currency", string="Currency",readonly=True)
+    # product_uom = fields.Char(string='UOM', store=False)
 
     @api.multi
     def _compare_data(self):
         for sale_order_line in self:
+            sale_order_line.customer_name=sale_order_line.order_id.partner_id.name
+            sale_order_line.product_sku_ref=sale_order_line.product_id.product_tmpl_id.sku_code
             if sale_order_line.order_id.state != 'cancel':
-                sale_order_line.product_name = sale_order_line.product_id.product_tmpl_id.name
-                sale_order_line.product_sku = sale_order_line.product_id.product_tmpl_id.sku_code
-                sale_order_line.customer_name = sale_order_line.order_id.partner_id.name
-                sale_order_line.sale_order = sale_order_line.order_id.name
-                stock_picking = self.env['stock.picking'].search([('sale_id', '=', sale_order_line.order_id.id),('state', '=', 'done')])
-                if len(stock_picking) == 1:
-                    stock_move_line = self.env['stock.move.line'].search([('picking_id', '=', stock_picking.id),('product_id', '=', sale_order_line.product_id.id)])
-                    sale_order_line.product_uom = stock_move_line.product_uom_id.name
-                    sale_order_line.qty_delivered = sale_order_line.qty_delivered
-                    sale_order_line.delivered_date = stock_picking.scheduled_date
-                else:
-                    sale_order_line.delivered_date = None
-                    sale_order_line.qty_delivered = sale_order_line.qty_delivered
-                sale_order_line.unit_price = sale_order_line.price_unit
-                sale_order_line.total_price = sale_order_line.qty_delivered * sale_order_line.price_unit
+                stock_location=self.env['stock.location'].search([('name', '=', 'Output')])
+                if stock_location:
+                    stock_picking = self.env['stock.picking'].search([('sale_id', '=', sale_order_line.order_id.id),('state', '=', 'done'),('location_id','=',stock_location.id)])
+                    if stock_picking:
+                        sale_order_line.delivered_date = stock_picking.date_done
+                    else:
+                        sale_order_line.delivered_date = None
