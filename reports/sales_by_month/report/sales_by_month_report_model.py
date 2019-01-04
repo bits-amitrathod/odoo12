e6e01f741b48397fba590a51131d8d8bfce367c8
Javed Shaikh <JavedShaikh@BenchmarkItSolutions.Com>
2019-01-04 17:37:08 +0530

    sales_by_month_report_model changed.

--- reports/sales_by_month/report/sales_by_month_report_model.py
+++ reports/sales_by_month/report/sales_by_month_report_model.py
@@ -11,21 +11,11 @@ class ReportProductSaleByCount(models.AbstractModel):
     @api.model
     def get_report_values(self, docids, data=None):
 
-        if len(docids) == 1:
-            ids = "(" + str(docids[0]) + ")"
-        else:
-            ids = str(tuple(docids))
-        view='sales_by_month'
-        records = "select sbm.sku_code,sbm.p_name,concat(sbm.currency_symbol,' ',cast(sbm.product_price as varchar)) as product_price,sbm.total_sale_quantity, sbm.total_amount," \
-                  "concat(sbm.currency_symbol,' ',cast(sbm.total_amount as varchar)) as total_amount,sbm.start_date,sbm.end_date,sbm.currency_symbol  from  " + view +" as sbm where id in "  + ids
-        self._cr.execute(records)
-        records = self._cr.fetchall()
-        for record in records:
-            record
+        records = self.env['sales_by_month'].search([('id', 'in', docids)])
 
         popup = self.env['salesbymonth.popup'].search([('create_uid', '=', self._uid)], limit=1, order="id desc")
 
-        if popup.compute_at_date:
+        if popup.start_date and popup.end_date and not popup.start_date is None and not popup.end_date is None:
             date = datetime.strptime(popup.start_date, '%Y-%m-%d').strftime('%m/%d/%Y') + " - " + datetime.strptime(
                 popup.end_date, '%Y-%m-%d').strftime('%m/%d/%Y')
         else:
