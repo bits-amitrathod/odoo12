af2171434619b9ce3c1bdf7263892a2b495a7258
Javed Shaikh <JavedShaikh@BenchmarkItSolutions.Com>
2019-01-07 19:51:20 +0530

    pricing rule report updated.

--- reports/lot_history/__manifest__.py
+++ reports/lot_history/__manifest__.py
@@ -17,7 +17,7 @@
     'version': '1.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','stock'],
+    'depends': ['base','stock','prioritization_engine'],
 
     # always loaded
     'data': [
