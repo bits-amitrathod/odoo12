9f589a9a3364c91c818da1faf654e288c430dcab
Javed Shaikh <JavedShaikh@BenchmarkItSolutions.Com>
2019-01-14 15:19:17 +0530

    report work in progress.

--- reports/broker_report/models/popup_view_model.py
+++ reports/broker_report/models/popup_view_model.py
@@ -12,7 +12,7 @@ class BrokerReportPopUp(models.TransientModel):
         (1, 'Date Range ')
     ], string="Compute", help="Choose to analyze the Show Summary or from a specific date in the past.",default=0)
     start_date = fields.Date('Start Date', help="Choose a date to get the Discount Summary at that  Start date",
-                             default=lambda self: self._get_current_date())
+                             default=(fields.date.today() - datetime.timedelta(days=31)))
     end_date = fields.Date('End Date', help="Choose a date to get the Discount Summary at that  End date",
                            default=fields.Date.today())
 
