effdc775070020c6d42b87da0fc3a5c18ebad878
amit <you@example.com>
2019-01-25 18:20:16 +0530

    Notification changes

--- inventory_notification/models/StockPicking.py
+++ inventory_notification/models/StockPicking.py
@@ -13,7 +13,18 @@ class StockPicking(models.Model):
     def button_validate(self):
         print("Inside Notifiction")
         #result=super(StockPicking,self).button_validate()
-        '''print("After Notifiction button validation")
+        print(self.move_type)
+        print(self.picking_type_id.name)
+        print("After Notifiction button validation")
         inv_notification = self.env['inventory.notification.scheduler'].search([])
-        inv_notification.pick_notification_for_customer(self)
-        print("After shipment_notification_for_user function")'''
+        if self.picking_type_id.name=='Pick':
+            inv_notification.pick_notification_for_customer(self)
+        elif self.picking_type_id.name=='Pack' or self.picking_type_id.name=='Pull':
+            inv_notification.pull_notification_for_user(self)
+            print("Pull notification for inventory manager")
+        elif self.picking_type_id.name == 'Delivery Orders':
+            inv_notification.out_notification_for_sale(self)
+            print("After shipment_notification_for_user function")
+
+
+
