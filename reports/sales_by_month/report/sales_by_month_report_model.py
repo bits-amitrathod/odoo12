7353c7b433f310dfbd1c39865bb4cdfd04b188d0
Tushar <tushargodase@benchmarkitsolutions.com>
2019-01-25 19:04:39 +0530

    sudo pull changes

--- reports/sales_by_month/report/sales_by_month_report_model.py
+++ reports/sales_by_month/report/sales_by_month_report_model.py
@@ -1,6 +1,7 @@
 import logging
 from odoo import api, fields, models
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 log = logging.getLogger(__name__)
 
@@ -11,16 +12,29 @@ class ReportProductSaleByCount(models.AbstractModel):
     @api.model
     def get_report_values(self, docids, data=None):
 
-        records = self.env['sales_by_month'].search([('id', 'in', docids)])
-
+        records = self.env['product.product'].search([('id', 'in', docids)])
         popup = self.env['salesbymonth.popup'].search([('create_uid', '=', self._uid)], limit=1, order="id desc")
+        today = datetime.date(datetime.strptime(popup.end_date, "%Y-%m-%d"))
+        end_of_month = today + relativedelta(day=1,months=1, days=-1)
+        sixth_month = (today - relativedelta(day=1, months=5))
 
-        if popup.start_date and popup.end_date and not popup.start_date is None and not popup.end_date is None:
-            date = datetime.strptime(popup.start_date, '%Y-%m-%d').strftime('%m/%d/%Y') + " - " + datetime.strptime(
-                popup.end_date, '%Y-%m-%d').strftime('%m/%d/%Y')
+        if popup.end_date and not popup.end_date is None:
+            date = sixth_month.strftime('%m/%d/%Y') + " - " + end_of_month.strftime('%m/%d/%Y')
         else:
             date = False
-
+        month6=(today - relativedelta(months=5)).strftime('%b-%Y')
+        month5 = (today - relativedelta(months=4)).strftime('%b-%Y')
+        month4 = (today - relativedelta(months=3)).strftime('%b-%Y')
+        month3 = (today - relativedelta(months=2)).strftime('%b-%Y')
+        month2 = (today - relativedelta(months=1)).strftime('%b-%Y')
+        month1 = (today).strftime('%b-%Y')
         return {
             'data': records,
-            'date': date}
+            'date': date,
+            'month1':month1,
+            'month2':month2,
+            'month3':month3,
+            'month4':month4,
+            'month5':month5,
+            'month6' :month6
+        }
