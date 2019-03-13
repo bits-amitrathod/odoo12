2f60410e42b981b7af971eccdb9618b3cb574449
prathu <prathameshdeshpande@benchmarkitsolutions.com>
2019-03-13 17:43:41 +0530

    Adding portal access module | inventory valuation summary print issue fix gross sales by product SKU column issue fix

---
+++ portal_access_management/models/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, SUPERUSER_ID
+#
+#
+# class portal_management(models.Model):
+#     _name = 'portal.management'
+#
+#     name = fields.Char("Name")
+#     value = fields.Integer("Value")
+
+
+
+
+
+
