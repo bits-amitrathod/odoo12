8581d4d66e132192d9748f474bd06dcf6ff75d4e
Ajinkya Nimbalkar <ajinkyanimbalkar@benchmarkitsolutions.com>
2019-04-12 21:56:24 +0530

    In stock scheduler disabled

--- inventory_notification/models/inventory_notification_scheduler.py
+++ inventory_notification/models/inventory_notification_scheduler.py
@@ -28,7 +28,7 @@ class InventoryNotificationScheduler(models.TransientModel):
     @api.multi
     def process_notification_scheduler(self):
         _logger.info("process_notification_scheduler called")
-        self.process_in_stock_scheduler()
+        # self.process_in_stock_scheduler()
         self.process_new_product_scheduler()
         self.process_notify_available()
         self.process_packing_list()
