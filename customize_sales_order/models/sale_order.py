94f2c30177aa6e182c0d44bd79b50fc34125ebea
Tushar <tushargodase@benchmarkitsolutions.com>
2019-05-27 05:56:26 -0400

    added tracking ref on sale order and also added filter by tracking reference

--- customize_sales_order/models/sale_order.py
+++ customize_sales_order/models/sale_order.py
@@ -8,6 +8,19 @@ class sale_order(models.Model):
 
     sale_note = fields.Text('Sale Notes')
 
+    carrier_track_ref = fields.Char('Tracking Reference', store=True, readonly=True, compute='_get_carrier_tracking_ref')
+
+    @api.one
+    def _get_carrier_tracking_ref(self):
+        for so in self:
+            stock_picking = self.env['stock.picking'].search([('origin', '=', so.name), ('picking_type_id', '=', 5),
+                                                              ('state', 'in', ['done', 'waiting', 'assigned'])])
+            for sp in stock_picking:
+                if sp.carrier_tracking_ref:
+                    so.carrier_track_ref = sp.carrier_tracking_ref
+                    break
+            break
+
 class stock_picking(models.Model):
     _inherit = 'stock.picking'
 
@@ -17,9 +30,3 @@ class stock_picking(models.Model):
         for stock_picking in self:
             sale_order = self.env['sale.order'].search([('name', '=', stock_picking.origin)])
             stock_picking.note = sale_order.sale_note
-
-
-
-
-
-
