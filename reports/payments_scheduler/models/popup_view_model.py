b01c4e92474d6245eba6c2cabd104536f057d700
Tushar Godse <tusharg@bitsdev.in>
2019-05-30 14:07:07 +0530

    Add Filtter By Default

--- reports/payments_scheduler/models/popup_view_model.py
+++ reports/payments_scheduler/models/popup_view_model.py
@@ -4,8 +4,7 @@ import datetime
 
 class  AcquisitionReportPopUp(models.TransientModel):
     _name = 'payments_scheduler.report.summary'
-    start_date = fields.Date('Start Date', help="Choose a date to get the Discount Summary at that  Start date",
-                                 default=(fields.date.today() - datetime.timedelta(days=31)))
+
     end_date = fields.Date('End Date', help="Choose a date to get the Discount Summary at that  End date",
                                default=fields.Datetime.now)
 
@@ -13,7 +12,7 @@ class  AcquisitionReportPopUp(models.TransientModel):
         tree_view_id = self.env.ref('payments_scheduler.form_list').id
         form_view_id = self.env.ref('payments_scheduler.form_view').id
 
-        ai = self.env['account.invoice'].search([('date_invoice','>=',self.start_date),('date_invoice','<=',self.end_date)]).ids
+        ai = self.env['account.invoice'].search([('date_invoice','<=',self.end_date)]).ids
 
         action = {
             'type': 'ir.actions.act_window',
@@ -22,7 +21,7 @@ class  AcquisitionReportPopUp(models.TransientModel):
             'name': _('Payments Scheduled'),
             'res_model': 'account.invoice',
             'domain': [('type', '=', 'in_invoice'),('id', 'in', ai)],
-            "context" : {'default_type': 'in_invoice', 'type': 'in_invoice'}  #, 'journal_type': 'purchase'
+            "context" : {'default_type': 'in_invoice', 'type': 'in_invoice','search_default_unpaid': 1}  #, 'journal_type': 'purchase'
         }
 
         return action
