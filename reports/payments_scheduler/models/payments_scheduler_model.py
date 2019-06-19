3ed9f5a848d663ec393ba133128f90a6f8708df2
Tushar Godse <tusharg@bitsdev.in>
2019-06-19 19:41:17 +0530

    payment shedular remove open fillter error

--- reports/payments_scheduler/models/payments_scheduler_model.py
+++ reports/payments_scheduler/models/payments_scheduler_model.py
@@ -12,19 +12,19 @@ class accoun_invoicr_changes(models.Model):
     def _compute_data(self):
         for sp in self:
             inv_cntact = None;
-            address = None;
+            address = "";
             for contact in  sp.partner_id.child_ids:
                 if contact.type == 'invoice':
                     inv_cntact = contact
                     break
-            if inv_cntact is not None:
+            if inv_cntact is not None :
                 sp.pay_to = inv_cntact.name
                 if inv_cntact.street: address = inv_cntact.street
                 if inv_cntact.street2: address = address + ', ' + inv_cntact.street2
                 if inv_cntact.city: address = address + ', ' + inv_cntact.city
-                if inv_cntact.state_id: address = address + ', ' + inv_cntact.state_id.name
+                if inv_cntact.state_id and inv_cntact.state_id.name : address = address + ', ' + inv_cntact.state_id.name
                 if inv_cntact.zip: address = address + ', ' + inv_cntact.zip
-                if inv_cntact.country_id: address = address + ', ' + inv_cntact.country_id.name
+                if inv_cntact.country_id   and inv_cntact.country_id.name : address = address + ', ' + inv_cntact.country_id.name
             if address is not None: sp.address = address
             purchase_order = self.env["purchase.order"].search([('name', '=', sp.origin)])
             if purchase_order :
