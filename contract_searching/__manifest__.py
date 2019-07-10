baa3671cf3b99b057930c6ec7a806d13862150ec
prathu <prathameshdeshpande@benchmarkitsolutions.com>
2019-07-10 17:07:30 +0530

    latest cr update

--- contract_searching/__manifest__.py
+++ contract_searching/__manifest__.py
@@ -1,24 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Sales Order Line Alert Message",
-
+    'name': "Contract Searching",
 
     'author': "Benchmark IT Solutions",
     'website': "http://www.benchmarkitsolutions.com",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['sale_stock','vendor_offer'],
-
-    # always loaded
+    'depends': ['base'],
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'demo/data.xml',
     ],
-    'installable': True,
 
+    'application': True,
+    'auto-install': False,
+    'installable': True,
 }
