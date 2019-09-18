6a26dc7040eebcb2a930977d64511e1a0ef81114
amit <you@example.com>
2019-09-18 16:36:37 +0530

    execute query module change

--- ms_query/__manifest__.py
+++ ms_query/__manifest__.py
@@ -1,9 +1,16 @@
 {
     "name"          : "Execute Query",
-    'version'       : '11.0.0.1',
-    "author"        : "Benchmark IT Solutions",
-    "category"      : "Report",
+    "version"       : "1.0",
+    "author"        : "Miftahussalam",
+    "website"       : "https://miftahussalam.com",
+    "category"      : "Extra Tools",
+    "license"       : "LGPL-3",
+    "support"       : "me@miftahussalam.com",
     "summary"       : "Execute query from database",
+    "description"   : """
+        Execute query without open postgres
+Goto : Settings > Technical
+    """,
     "depends"       : [
         "base",
         "mail",
@@ -15,9 +22,10 @@
     "demo"          : [],
     "test"          : [],
     "images"        : [
-                        "static/description/images/main_1.png",
-                        "static/description/images/main_2.png",
-                        "static/description/images/main_3.png",
+        "static/description/images/main_screenshot.png",
+        "static/description/images/main_1.png",
+        "static/description/images/main_2.png",
+        "static/description/images/main_3.png",
     ],
     "qweb"          : [],
     "css"           : [],
