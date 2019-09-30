5527ffe6ffd3680a4c7b1086d171c8c62a4f6d63
amit <you@example.com>
2019-09-30 15:52:16 +0530

    inv notification min amd max issue

--- inventory_notification/models/inventory_notification_scheduler.py
+++ inventory_notification/models/inventory_notification_scheduler.py
@@ -2253,14 +2253,14 @@ class InventoryNotificationScheduler(models.TransientModel):
                 coln_name.append(column_name)
                 if column_name == 'minExDate':
                     if query_result and query_result['min']:
-                        column = query_result['min'].strftime('%m/%d/%Y')
-                        #column = datetime.strptime(query_result['min'], "%Y-%m-%d %H:%M:%S").strftime('%m/%d/%Y')
+                        min=str(query_result['min'])
+                        column = datetime.strptime(min, "%Y-%m-%d %H:%M:%S").strftime('%m/%d/%Y')
                     else:
                         column = ""
                 elif column_name == 'maxExDate':
                     if query_result and query_result['max']:
-                        column = query_result['max'].strftime('%m/%d/%Y')
-                        #column = datetime.strptime(query_result['max'], "%Y-%m-%d %H:%M:%S").strftime('%m/%d/%Y')
+                        max=str(query_result['max'])
+                        column = datetime.strptime(max, "%Y-%m-%d %H:%M:%S").strftime('%m/%d/%Y')
                     else:
                         column = ""
                 elif column_name == 'customer_price_list':
