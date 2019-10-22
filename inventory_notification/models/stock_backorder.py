16d7d6530af59f53ef9701bdb7774afa04766494
Tushar Godse <tusharg@bitsdev.in>
2019-10-22 19:01:16 +0530

    backorder and cancel so low stock email

--- inventory_notification/models/stock_backorder.py
+++ inventory_notification/models/stock_backorder.py
@@ -22,6 +22,10 @@ class StockBackorder(models.TransientModel):
                 elif picking.picking_type_id.name == 'Delivery Orders' and picking.state=='done':
                     _logger.info(" Delivery Orders ******** backorder ***** Start********")
                     inv_notification.out_notification_for_sale(picking)
+                    _logger.info(" Delivery Orders ********low Stock ***** Start ********")
+                    product_ids = self.unique(self.env['stock.move.line'].search([('picking_id', '=', picking.id)]))
+                    inv_notification.process_notify_low_stock_products(product_ids)
+                    _logger.info(" Delivery Orders ********low Stock ***** End ********")
                     _logger.info(" Delivery Orders ******** backorder ***** End ********")
             # elif picking.purchase_id:
             #     if picking.picking_type_id.name == 'Receipts' and  picking.state == 'done':
@@ -45,8 +49,22 @@ class StockBackorder(models.TransientModel):
                 elif picking.picking_type_id.name == 'Delivery Orders' and picking.state=='done':
                     _logger.info(" Delivery Orders ******** cancel_backorder ***** Start********")
                     inv_notification.out_notification_for_sale(picking)
+                    _logger.info(" Delivery Orders ********low Stock ***** Start ********")
+                    product_ids = self.unique(self.env['stock.move.line'].search([('picking_id', '=', picking.id)]))
+                    inv_notification.process_notify_low_stock_products(product_ids)
+                    _logger.info(" Delivery Orders ********low Stock ***** End ********")
                     _logger.info(" Delivery Orders ******** cancel_backorder ***** End********")
             # elif picking.purchase_id:
             #     if picking.picking_type_id.name == 'Receipts' and  picking.state == 'done':
             #         inv_notification.po_receive_notification_for_acquisitions_manager(picking)
         return action
+
+    def unique(self,list1):
+        # intilize a null list
+        unique_list = []
+        # traverse for all elements
+        for x in list1:
+            # check if exists in unique_list or not
+            if x.product_id not in unique_list:
+                unique_list.append(x.product_id)
+        return unique_list
