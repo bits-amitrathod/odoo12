fd4470897a679e95dd0019f5921b90d3cbf186bb
bits-rajendrakhatal <amolshirude@benchmarkitsolutions.com>
2019-12-05 14:17:46 +0530

    update log when available qty is zero

--- customer-requests/models/DocumentProcessTransientModel.py
+++ customer-requests/models/DocumentProcessTransientModel.py
@@ -158,9 +158,13 @@ class DocumentProcessTransientModel(models.TransientModel):
 
         if sps_customer_product_priority >= 0:
             available_qty = self.env['available.product.dict'].get_available_product_qty(user_id, product_id, expiration_tolerance)
+            customer_request_logs = ''
+            if int(available_qty) == 0:
+                customer_request_logs = 'As per requested expiration tolerance product lot not available.'
             req.update(dict(product_id=product_id, status='New', priority=sps_customer_product_priority, auto_allocate=auto_allocate,
                             min_threshold=min_threshold, max_threshold=max_threshold, cooling_period=cooling_period, length_of_hold=length_of_hold,
-                            expiration_tolerance=expiration_tolerance, partial_ordering=partial_ordering, partial_UOM=partial_uom, available_qty=available_qty))
+                            expiration_tolerance=expiration_tolerance, partial_ordering=partial_ordering, partial_UOM=partial_uom,
+                            available_qty=available_qty, customer_request_logs=customer_request_logs))
             return True
         else:
             return False
