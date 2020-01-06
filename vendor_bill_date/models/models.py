b752db3305322543f49f66675035ca645be1d7de
tisoard <56162576+tisoard@users.noreply.github.com>
2020-01-06 14:11:45 -0500

    Update models.py

--- vendor_bill_date/models/models.py
+++ vendor_bill_date/models/models.py
@@ -29,7 +29,8 @@ class VendorBillDate(models.Model):
             pterm_list = pterm.with_context(currency_id=self.company_id.currency_id.id).compute(value=1, date_ref=self.date_invoice)[0]
             self.date_due = max(line[0] for line in pterm_list)
         elif self.date_due #and (self.date_invoice > self.date_due):
-            self.date_due = self.date_invoice
+            if self.date_invoice > self.date_due:
+                self.date_due = self.date_invoice
 
 
     # Populates Due_Date at the time of Saving bill of purchase order in Purchase module when click on 'Save' button
