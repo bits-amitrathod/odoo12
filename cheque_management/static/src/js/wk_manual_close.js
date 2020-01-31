e3d803f87564dd0c38da9cfd2389bb5b9f875b4c
tisoard <56162576+tisoard@users.noreply.github.com>
2020-01-31 10:48:00 -0500

    Revert "Merge pull request #6 from bits-amitrathod/cheque-managment"
    This reverts commit 98d7b547006782f9cd50e517a253ab909347bd30, reversing
    changes made to cb4c5f653e0d66827a1d04d1b2545bb59535842f.

--- cheque_management/static/src/js/wk_manual_close.js
+++
@@ -1,9 +0,0 @@
-odoo.define('odoo_cheque_management.wk_manual_close', function (require) {
-    'use strict';
-    console.log("-------Backend------------------");
-    // $(document).find('.wk_manual_close').on('click', function (e) { 
-    $(document).on('click', '.wk_manual_close', function (event){
-        console.log("-------------wk_manual_close---------------");
-        $('.wk_cancel_btn').click();
-    });
-});
