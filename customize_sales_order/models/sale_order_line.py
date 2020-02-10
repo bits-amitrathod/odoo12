80ba7dd6bdb10b92788ed718ccfb43659e3795b8
amolshirude <amolshirude@benchmarkitsolutions.com>
2020-02-10 15:16:06 +0530

    Items that are deleted from Stockhawk quote are not actually being deleted issue resolved

--- customize_sales_order/models/sale_order_line.py
+++ customize_sales_order/models/sale_order_line.py
@@ -6,9 +6,9 @@ from odoo.exceptions import UserError
 class SaleOrderLineInherit(models.Model):
     _inherit = 'sale.order.line'
 
-    @api.multi
-    def unlink(self):
-        if self.filtered(lambda line: line.state in 'done' and (line.invoice_lines or not line.is_downpayment)):
-            raise UserError(_('You can not remove an order line once the sales order is confirmed.\nYou should rather set the quantity to 0.'))
-        return models.Model.unlink(self)
+    # @api.multi
+    # def unlink(self):
+    #     if self.filtered(lambda line: line.state in 'done' and (line.invoice_lines or not line.is_downpayment)):
+    #         raise UserError(_('You can not remove an order line once the sales order is confirmed.\nYou should rather set the quantity to 0.'))
+    #     return models.Model.unlink(self)
 
