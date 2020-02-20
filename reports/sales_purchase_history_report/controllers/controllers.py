a5d56a1ea0bfa43254ec523ef048d9979102ef1f
Anvay Walujkar <anvayw@bitsdev.in>
2020-02-20 16:29:32 +0530

    Optimized sale purchase history , sales quotation history, in stock

--- reports/sales_purchase_history_report/controllers/controllers.py
+++ reports/sales_purchase_history_report/controllers/controllers.py
@@ -34,13 +34,12 @@ class ReportPrintSalesPurchaseHistory(http.Controller):
 
             for i, fieldname in enumerate(field):
                 worksheet.write(0, i, fieldname)
-                if i == 4:
+                if i == 5:
                     worksheet.col(i).width = 15500  #
                 elif i == 1:
                     worksheet.col(i).width = 10000  #
                 else:
                     worksheet.col(i).width = 4700  # around 110 pixels
-
 
             base_style = xlwt.easyxf('align: wrap yes')
             date_style = xlwt.easyxf('align: wrap yes', num_format_str='YYYY-MM-DD')
@@ -79,44 +78,11 @@ class ReportPrintSalesPurchaseHistory(http.Controller):
 
     @http.route('/web/export/sale_purchase_history_export', type='http', auth="public")
     @serialize_exception
-    def download_document_xl(self,token=1, debug=1):
+    def download_document_xl(self, token=1, debug=1):
 
         str_functions_old = """	 
-          
-            select pp.default_code as sku,rp.name cust_name,so.name so,pt.name prod_name,sp.date_done,
-            round( sm.product_uom_qty, 2) as qty ,um.name as uom,
-            round( sol.price_unit, 2) as price_unit ,round((sm.product_uom_qty * sol.price_unit),2) as total,
-            CASE   WHEN quotations_per_code.quotation_count IS NULL THEN '0' 
-                             ELSE quotations_per_code.quotation_count
-                           END     AS quotation_count
-            from sale_order_line  sol 
-            left join sale_order so on sol.order_id = so.id 
-            
-            left join product_product pp on sol.product_id = pp.id
-            left join product_template pt on pp.product_tmpl_id = pt.id
-            left join res_partner rp on so.partner_id = rp.id
-            left join stock_picking sp on sp.sale_id=so.id 
-            left join stock_move sm on sm.picking_id=sp.id and  sm.product_id = pp.id
-            left join uom_uom um on um.id = sm.product_uom
-            
-            left join (SELECT ppc.id,count(ppc.id) as quotation_count 
-                                    from  product_product ppc   
-                                       INNER JOIN sale_order_line soli ON soli.product_id=ppc.id 
-                                       INNER JOIN product_template pti ON  pti.id=ppc.product_tmpl_id 
-                                       INNER JOIN sale_order sor ON sor.id=soli.order_id   
-                                        where sor.state in ('draft','sent')   
-                                           GROUP  BY ppc.id) AS quotations_per_code
-                                           on pp.id =  quotations_per_code.id  
-            
-            where so.state not in ('cancel','void') and sol.price_unit > 0 and sp.state ='done' 
-            and sp.location_dest_id in  (select id from stock_location where name='Customers' order by id desc  limit 1)
-            and sm.product_uom_qty > 0
-                            
-        """
 
-        str_functions = """	 
-
-                    select pp.default_code as sku,rp.name cust_name,so.name so,pt.name prod_name,sp.date_done,
+           select pp.default_code as sku,rp.name cust_name,so.name so,pt.name prod_name,sp.date_done,
                     round( sol.qty_delivered, 2) as qty ,um.name as uom,
                     round( sol.price_unit, 2) as price_unit ,round((sol.qty_delivered * sol.price_unit),2) as total,
                     CASE   WHEN quotations_per_code.quotation_count IS NULL THEN '0' 
@@ -127,14 +93,14 @@ class ReportPrintSalesPurchaseHistory(http.Controller):
 			where rus.id=rp.account_manager_cust limit 1 ) as account_mang
                     from sale_order_line  sol 
                     left join sale_order so on sol.order_id = so.id 
-                    
+
                     left join product_product pp on sol.product_id = pp.id
                     left join product_template pt on pp.product_tmpl_id = pt.id
                     left join res_partner rp on so.partner_id = rp.id
                     left join stock_picking sp on sp.sale_id=so.id 
-                  
+
                     left join uom_uom um on um.id = sol.product_uom
-                    
+
                     left join (SELECT ppc.id,count(ppc.id) as quotation_count 
                                             from  product_product ppc   
                                                INNER JOIN sale_order_line soli ON soli.product_id=ppc.id 
@@ -143,13 +109,57 @@ class ReportPrintSalesPurchaseHistory(http.Controller):
                                                 where sor.state in ('draft','sent')   
                                                    GROUP  BY ppc.id) AS quotations_per_code
                                                    on pp.id =  quotations_per_code.id  
-                    
+
+                    where so.state not in ('cancel','void') and sol.price_unit >= 0 and sp.state ='done' 
+                    and sp.location_dest_id in  (select id from stock_location where name='Customers' and active =true
+                     order by id desc )   and sol.qty_delivered > 0 and pp.active =true and pt.active =true
+
+        """
+
+        str_functions = """	 
+
+                    select pp.default_code as sku,rp.name cust_name,so.name so,pt.name prod_name,sp.date_done,
+                    round( sol.qty_delivered, 2) as qty ,um.name as uom,
+                    round( sol.price_unit, 2) as price_unit ,round((sol.qty_delivered * sol.price_unit),2) as total,
+                    CASE   WHEN quotations_per_code.quotation_count IS NULL THEN '0' 
+                                     ELSE quotations_per_code.quotation_count
+                                   END     AS quotation_count,
+
+				rp.is_share,rp.is_broker,rp.sale_margine as sale_level,cr.name as sales_team,
+				account_mang.name as account_mang,sales_person.name as sales_person
+                    from sale_order_line  sol 
+                    left join sale_order so on sol.order_id = so.id 
+
+                    left join product_product pp on sol.product_id = pp.id
+                    left join product_template pt on pp.product_tmpl_id = pt.id
+                    left join res_partner rp on so.partner_id = rp.id
+                    left join stock_picking sp on sp.sale_id=so.id 
+
+                    left join uom_uom um on um.id = sol.product_uom
+					left join crm_team cr on cr.id = so.team_id
+
+					left join (select rps2.name,rps.id from res_users rus join res_partner rps on rus.id=rps.account_manager_cust 
+					right join res_partner rps2 on rps2.id = rus.partner_id 
+					where rps.active=true and rus.active=true) as account_mang on  account_mang.id=rp.id
+
+				    left join (select rps2.name,rps.id from res_users rus join res_partner rps on rus.id=rps.user_id 
+					right join res_partner rps2 on rps2.id = rus.partner_id 
+					where rps.active=true and rus.active=true) as sales_person on  sales_person.id=rp.id
+
+                    left join (SELECT ppc.id,count(ppc.id) as quotation_count 
+                                            from  product_product ppc   
+                                               INNER JOIN sale_order_line soli ON soli.product_id=ppc.id 
+                                               INNER JOIN product_template pti ON  pti.id=ppc.product_tmpl_id 
+                                               INNER JOIN sale_order sor ON sor.id=soli.order_id   
+                                                where sor.state in ('draft','sent')   
+                                                   GROUP  BY ppc.id) AS quotations_per_code
+                                                   on pp.id =  quotations_per_code.id  
+
                     where so.state not in ('cancel','void') and sol.price_unit >= 0 and sp.state ='done' 
                     and sp.location_dest_id in  (select id from stock_location where name='Customers' and active =true
                      order by id desc )   and sol.qty_delivered > 0 and pp.active =true and pt.active =true
 
                 """
-
 
         request.env.cr.execute(str_functions)
         order_lines = request.env.cr.dictfetchall()
@@ -157,14 +167,17 @@ class ReportPrintSalesPurchaseHistory(http.Controller):
         records = []
 
         for line in order_lines:
-
-            records.append([line['sku'], line['cust_name'],line['account_mang'], line['so'], line['prod_name'],
-                            line['date_done'], line['qty'], line['uom'],line['price_unit'],
-                            line['total'],line['quotation_count']])
+            records.append([line['sku'], line['cust_name'], line['sales_person'], line['account_mang'], line['so'],
+                            line['prod_name'],
+                            line['is_broker'], line['is_share'], line['sales_team'], line['sale_level'],
+                            line['date_done'], line['qty'], line['uom'], line['price_unit'],
+                            line['total'], line['quotation_count']])
 
         res = request.make_response(
-            self.from_data(["Product SKU", "Customer Name","Account Manager ", "Sales Order#", "Product Name", "Delivered Date"
-                               , "Delivered Qty", "UOM", "Unit Price", "Total","Open Quotations Per Code"],
+            self.from_data(["Product SKU", "Customer Name", "Sales Person", "Account Manager ", "Sales Order#",
+                            "Product Name", "Is Broker", "Is Shared", "Sales Team", "Sales Level",
+                            "Delivered Date"
+                               , "Delivered Qty", "UOM", "Unit Price", "Total", "Open Quotations Per Code"],
                            records),
             headers=[('Content-Disposition', content_disposition('sale_purchase_history' + '.xls')),
                      ('Content-Type', 'application/vnd.ms-excel')],
