7d12ce9ed0bec57e91000c1534d37d0ee40484b1
amolshirude <amolshirude@benchmarkit.solutions>
2020-08-06 18:51:00 +0530

    added national account in sales and quotation

--- customize_sales_order/models/sale_order.py
+++ customize_sales_order/models/sale_order.py
@@ -31,6 +31,8 @@ class sale_order(models.Model):
     account_manager = fields.Many2one('res.users', store=True, readonly=True, string="Key Account", compute="get_account_manager")
     user_id = fields.Many2one('res.users', string='Business Development', index=True, track_visibility='onchange',
                               track_sequence=2, default=lambda self: self.env.user)
+    national_account = fields.Many2one('res.users', readonly=True, string="National Account",
+                                       related='partner_id.national_account_rep')
 
     @api.one
     def get_account_manager(self):
