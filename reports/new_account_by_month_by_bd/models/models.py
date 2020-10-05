0b6610ddd5d296471294e59d7437d62f998e1b2e
amolshirude <amolshirude@benchmarkit.solutions>
2020-10-05 22:39:33 +0530

    updated report

--- reports/new_account_by_month_by_bd/models/models.py
+++ reports/new_account_by_month_by_bd/models/models.py
@@ -11,6 +11,7 @@ _logger = logging.getLogger(__name__)
 class NewAccountByMonthByBd(models.Model):
     _name = 'report.bd.new.account'
     _auto = False
+    # _order = 'business_development'
 
     onboard_date = fields.Datetime('Onboard Date')
     customer = fields.Many2one('res.partner', 'Customer Name')
@@ -25,43 +26,103 @@ class NewAccountByMonthByBd(models.Model):
 
         start_date = self.env.context.get('start_date')
         end_date = self.env.context.get('end_date')
+        business_development_id = self.env.context.get('business_development')
+
         if start_date and end_date :
             select_query = """
-                       SELECT
-                            ROW_NUMBER () OVER (ORDER BY RP.id) AS id,
-                            RP.id                               AS customer,
-                            COALESCE(RP.reinstated_date, RP.create_date)                  AS onboard_date,
-                            RP.user_id                          AS business_development
-            
-                       FROM public.res_partner RP
-            
-                        WHERE RP.customer = true AND RP.active = true 
-                        AND RP.user_id IS NOT NULL AND RP.parent_id IS NULL
-            
-            
+                SELECT 
+                    ROW_NUMBER () OVER (ORDER BY RPS.id)    AS id,
+                    SOS.partner_id                          AS customer, 
+                    SOS.user_id                             AS business_development, 
+                    MIN(SPS.date_done)                      AS onboard_date
+                FROM public.sale_order SOS 
+                INNER JOIN 
+                    public.res_partner RPS 
+                ON 
+                    SOS.partner_id = RPS.id AND (RPS.is_wholesaler is NULL OR RPS.is_wholesaler != TRUE) 
+                    AND (RPS.is_broker is NULL OR RPS.is_broker != TRUE)
+                INNER JOIN 
+                    public.stock_picking SPS 
+                ON 
+                    SOS.id = SPS.sale_id AND SPS.picking_type_id = 5 AND SPS.state = 'done'
+                
+                WHERE SOS.user_id IS NOT NULL AND SOS.state NOT IN ('cancel', 'void') AND SOS.partner_id IN
+                
+                ((  SELECT 
+                        DISTINCT (SO.partner_id) partner1
+                    FROM 
+                        public.sale_order SO
+                    INNER JOIN 
+                        public.stock_picking SP 
+                    ON 
+                        SO.id = SP.sale_id AND SP.state = 'done' AND SP.picking_type_id = 5 AND
+                                  
                    """
+            select_query = select_query + " SP.date_done BETWEEN '" + str(start_date) + "'" + " AND '" + str(
+                end_date) + "' " + """ WHERE SO.state NOT IN ('cancel', 'void') AND SO.partner_id NOT IN (
+                SELECT 
+                    DISTINCT (SO.partner_id) partner
+                FROM 
+                    public.sale_order SO
+                INNER JOIN 
+                    public.stock_picking SP ON SO.id = SP.sale_id AND SP.state = 'done' AND 
+                    SP.picking_type_id = 5 AND SP.date_done <= ' """ + str(start_date) + """ ' 
+                    WHERE SO.state NOT IN ('cancel', 'void'))) UNION ALL ("""
 
-            # select_query = select_query + " AND RP.create_date >= COALESCE(RP.reinstated_date, RP.create_date) " \
-            #                               " AND RP.create_date BETWEEN '" + str(start_date) + "' " + " AND '" + str(
-            #     end_date) + "' "
-            
-            select_query = select_query + " AND COALESCE(RP.reinstated_date, RP.create_date) BETWEEN '" + str(start_date) + "' " + " AND '" + str(
-                end_date) + "' "
-            
-            
-            business_development_id = self.env.context.get('business_development')
-            
-            
+            select_query = select_query + """ 
+            SELECT 
+                DISTINCT (SO.partner_id) partner1
+            FROM 
+                public.sale_order SO
+            INNER JOIN 
+                public.stock_picking SP ON SO.id IN (
+                SELECT SO.id
+                FROM 
+                    public.sale_order SO
+                INNER JOIN 
+                    public.stock_picking SP 
+                ON 
+                    SO.id = SP.sale_id AND SP.state = 'done' AND SP.picking_type_id = 5 
+                              AND SO.state NOT IN ('cancel', 'void') AND SO.user_id IS NOT NULL
+                WHERE SO.partner_id IN (
+                        SELECT id
+                        FROM public.res_partner RP
+                        WHERE RP.reinstated_date IS NOT NULL AND RP.reinstated_date BETWEEN ' """ + \
+                        str(start_date) + "'" + " AND '" + str(end_date) + "' ) ) WHERE SP.date_done BETWEEN ' " + \
+                        str(start_date) + "'" + " AND '" + str(end_date) + "' AND SP.state = 'done' " \
+                        " AND SP.picking_type_id = 5 AND SO.state NOT IN ('cancel', 'void') AND SO.partner_id NOT IN ("
+
+            select_query = select_query + """ 
+                SELECT 
+                    DISTINCT (SO.partner_id) partner
+                FROM 
+                    public.sale_order SO        
+                INNER JOIN 
+                    public.stock_picking SP 
+                ON SO.id IN (        
+                        Select SO.id
+                        From public.sale_order SO
+                        INNER JOIN public.stock_picking SP 
+                        ON SO.id = SP.sale_id AND SP.state = 'done' AND SP.picking_type_id = 5 
+                                      AND SO.state NOT IN ('cancel', 'void') AND SO.user_id IS NOT NULL
+                        WHERE SO.partner_id IN (
+                                SELECT id 
+                                FROM public.res_partner RP
+                                WHERE RP.reinstated_date IS NOT NULL AND RP.reinstated_date <= '""" + str(start_date) +\
+                                "' ) ) WHERE SP.date_done <= '" + str(start_date) + "' AND SP.state = 'done' AND " \
+                                " SP.picking_type_id = 5 AND SO.state NOT IN ('cancel', 'void') ))) AND SPS.date_done" \
+                                " >= COALESCE(RPS.reinstated_date, RPS.create_date) AND SPS.date_done BETWEEN '" + \
+                                str(start_date) + "'" + " AND '" + str(end_date) + "' "
+
             if business_development_id:
-                select_query = select_query + "AND RP.user_id = '" + str(business_development_id) + "'"
-            
-            
-            # group_by = """ GROUP BY RP.id"""
-            
-            sql_query = select_query  #+ group_by
-            
-            self._cr.execute("CREATE VIEW " + self._name.replace(".", "_") + " AS ( " + sql_query + " )")
-            
+                select_query = select_query + "AND SOS.user_id = '" + str(business_development_id) + "'"
+
+            group_by = """ GROUP BY RPS.id, SOS.partner_id, SOS.user_id  
+                            ORDER BY RPS.name """
+
+            select_query = select_query + group_by
+
+            self._cr.execute("CREATE VIEW " + self._name.replace(".", "_") + " AS ( " + select_query + " )")
 
     @api.model_cr
     def delete_and_create(self):
@@ -102,7 +163,6 @@ class NewAccountByMonthByBdExport(models.TransientModel):
                 'url': '/web/export/new_account_by_month_by_bd_export/' + str(s_date) + '/' + str(e_date) + '/' + str('none'),
                 'target': 'new'
             }
-
 
     @staticmethod
     def string_to_date(date_string):
