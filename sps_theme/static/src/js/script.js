6eef5a157f783375cd767d6937e1b4eeb78864d7
Sagar Khese <sagark@bitsdev.in>
2020-11-12 19:22:28 +0530

    design for equipment sel and repair

---
+++ sps_theme/static/src/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $( "#birthdate" ).datepicker({
+        inline: true
+    });
+});
+
+   function getFileNameWithExt(event) {
+
+      if (!event || !event.target || !event.target.files || event.target.files.length === 0) {
+        return;
+      }
+
+      const name = event.target.files[0].name;
+      const lastDot = name.lastIndexOf('.');
+
+      const fileName = name.substring(0, lastDot);
+      const ext = name.substring(lastDot + 1);
+
+      outputfile.value = fileName;
+
+    }
+
