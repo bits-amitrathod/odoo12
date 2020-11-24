919603f9d45eea107af98e1999e345867156bb81
amolshirude <amolshirude@benchmarkit.solutions>
2020-11-24 11:26:40 +0530

    added log

--- vendor_offer/models/models.py
+++ vendor_offer/models/models.py
@@ -2440,7 +2440,7 @@ class CustomerACQManager(models.Model):
     acq_manager = fields.Many2one('res.users', string="ACQ Manager", domain="[('active', '=', True)"""
                                                                             ",('share','=',False)]")
 
-    vendor_email = fields.Char(string="Vendor Email")
+    vendor_email = fields.Char(string="Vendor Email", track_visibility='onchange')
 
 class MailComposer(models.TransientModel):
     _inherit = 'mail.compose.message'
