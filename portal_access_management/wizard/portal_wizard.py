8d8e6b8cce27305441da745b6005880fe58a5fe3
amolshirude <amolshirude@benchmarkit.solutions>
2021-03-26 19:04:02 +0530

    Removed db name from password reset template

---
+++ portal_access_management/wizard/portal_wizard.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import logging
+
+from odoo.tools.translate import _
+from odoo.exceptions import UserError
+
+from odoo import api, models
+
+
+_logger = logging.getLogger(__name__)
+
+# welcome email sent to portal users
+# (note that calling '_' has no effect except exporting those strings for translation)
+
+
+class PortalWizardUser(models.TransientModel):
+    """
+        A model to configure users in the portal wizard.
+    """
+
+    _inherit = 'portal.wizard.user'
+    _description = 'Portal User Config'
+
+    @api.multi
+    def _send_email(self):
+        """ send notification email to a new portal user """
+        if not self.env.user.email:
+            raise UserError(_('You must have an email address in your User Preferences to send emails.'))
+
+        # determine subject and body in the portal user's language
+        template = self.env.ref('portal_access_management.mail_template_data_portal_welcome_cstm')
+        for wizard_line in self:
+            lang = wizard_line.user_id.lang
+            partner = wizard_line.user_id.partner_id
+
+            portal_url = partner.with_context(signup_force_type_in_url='', lang=lang)._get_signup_url_for_action()[
+                partner.id]
+            partner.signup_prepare()
+
+            if template:
+                template.with_context(dbname=self._cr.dbname, portal_url=portal_url, lang=lang).send_mail(
+                    wizard_line.id, force_send=True)
+            else:
+                _logger.warning("No email template found for sending email to the portal user")
+
+        return True
