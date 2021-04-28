28e4ccde95249d6e7a46843b7c70a5a88a41bce1
Tushar Godase <tushargodase@benchmarkit.solutions>
2021-04-28 14:12:04 +0530

    blog post Changes

--- reports/account_closed_by_bd/__manifest__.py
+++ reports/account_closed_by_bd/__manifest__.py
@@ -1,32 +1,23 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Revenue From Accounts Closed In 12 Months By BD",
-
     'summary': """
       Report
        """,
-
-    'description': """
-
-    """,
-
+    'description': "",
     'author': "Benchmark IT Solutions",
     'website': "http://www.benchmarkitsolutions.com",
-
     'category': 'Report',
     'version': '1.0',
-
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale'],
-
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
-        'report/account_closed_by_bd_report.xml',
-        'report/account_closed_by_bd_temp.xml',
+        # 'views/views.xml',
+        # 'report/account_closed_by_bd_report.xml',
+        # 'report/account_closed_by_bd_temp.xml',
     ],
-
     'installable': True,
     'application': True,
     'auto_install': False,
