2fc2d264fbce55553db2c2d5b2a9a6d13f0bd6ae
Tushar Godase <tushargodase@benchmarkit.solutions>
2021-04-28 19:44:57 +0530

    payment Ache Done

--- reports/payments_scheduler/report/payments_scheduler_report_temp.py
+++ reports/payments_scheduler/report/payments_scheduler_report_temp.py
@@ -5,8 +5,8 @@ from odoo import api, fields, models
 class payments_scheduler_report(models.AbstractModel):
     _name = 'report.payments_scheduler.payments_scheduler_temp_test'
 
-    # @api.model
-    # def _get_report_values(self, docids, data=None):
-    #     data = self.env['account.invoice'].browse(docids)
-    #     action = {'data': data}
-    #     return action
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        data = self.env['account.move'].browse(docids)
+        action = {'data': data}
+        return action
