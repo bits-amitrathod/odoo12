5bcc420a4a79bdd35a2b195014056541cf6d838d
Adil khan <adil.khan771@webkul.com>
2021-06-29 21:29:58 +0530

    [ADD]website_webkul_addons

---
+++ website_webkul_addons/wizard/wk_website_wizard.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author      : Webkul Software Pvt. Ltd. (<https://webkul.com/>)
+# Copyright(c): 2015-Present Webkul Software Pvt. Ltd.
+# All Rights Reserved.
+#
+#
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#
+# You should have received a copy of the License along with this program.
+# If not, see <https://store.webkul.com/license.html/>
+#################################################################################
+from odoo import api, fields, models, _
+
+
+class WebsiteMessageWizard(models.TransientModel):
+
+    _name="website.message.wizard"
+    _description="Wizard for show message for user."
+
+    message = fields.Text(string="Message")
+
+    def update_latest_record(self):
+        active_model = self.env[self._context.get('active_model')]
+        active_id = self._context.get('active_id') or self._context.get('active_ids')[0]
+        for current_record in self:
+            is_active_record = active_model.search([('is_active','=',True)])
+            is_active_record.write({'is_active':False})
+            active_record = active_model.browse(active_id)
+            active_record.write({'is_active':True})
+        return True
+
+    def cancel(self):
+        active_model = self.env[self._context.get('active_model')]
+        active_id = self._context.get('active_id') or self._context.get('active_ids')[0]
+        active_record = active_model.browse(active_id)
+        active_record.write({'is_active':False})
