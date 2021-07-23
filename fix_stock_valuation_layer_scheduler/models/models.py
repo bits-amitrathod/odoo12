3bbd14b2ee2a7feae40c7686a0acf1b497e0c181
Anvay walujkar <anvaywalujkar@benchmarkit.solutions>
2021-07-23 13:58:06 +0530

    code for all prod

--- fix_stock_valuation_layer_scheduler/models/models.py
+++ fix_stock_valuation_layer_scheduler/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import time
 from odoo import models, fields, api, _
 
 _logger = logging.getLogger(__name__)
@@ -13,7 +14,14 @@ class FixStockValuationLayerScheduler(models.Model):
     def fix_stock_valuation_layer_scheduler(self,prods_sku):
         _logger.info('In fix stock valuation layer scheduler ')
         company = self.env.company
-        products = self.env['product.product'].search([('default_code', 'in', prods_sku)])
+        start_time = time.time()
+        count_of_prod = 0
+        rows_corrected = 0
+        if len(prods_sku) == 1 and prods_sku[0] == 'fixallprod':
+            products = self.env['product.product'].search([])
+        else:
+            products = self.env['product.product'].search([('default_code', 'in', prods_sku)])
+
         for product in products:
             prod_id = product.id
             unit_cost = 0
@@ -22,8 +30,6 @@ class FixStockValuationLayerScheduler(models.Model):
             value = 0
             new_value = 0
             value_at_prod = 0
-            unit_cost = product.standard_price
-            value_at_prod = product.actual_quantity * product.standard_price
             domain = [
                 ('product_id', '=', prod_id),
                 ('company_id', '=', company.id),
@@ -31,42 +37,48 @@ class FixStockValuationLayerScheduler(models.Model):
             groups = self.env['stock.valuation.layer'].read_group(domain, ['value:sum', 'quantity:sum'], ['product_id'])
 
             for group in groups:
-                value = self.env.company.currency_id.round(group['value'])
+                value = round(self.env.company.currency_id.round(group['value']),2)
                 quantity = group['quantity']
 
-            vals = {
-                'product_id': prod_id,
-                'unit_cost': unit_cost,
-                'quantity': 0,
-                'value': 0,
-                'remaining_qty': 0,
-                'company_id': company.id,
-                'description': 'Adjustment from fix stock valuation layer scheduler',
-            }
-            if quantity != product.actual_quantity:
-                print('run logic for qty offset')
-                new_quantity = product.actual_quantity - quantity
-                vals["quantity"] = new_quantity
-
-            if value != value_at_prod:
-                print('run logic for value offset')
-                new_value = value_at_prod - value
-                vals["value"] = new_value
-
-            if (quantity != product.actual_quantity) or (value != value_at_prod):
-                adjustment_value = self.env['stock.valuation.layer'].sudo().create(vals)
-
-            svls_to_fix = self.env['stock.valuation.layer'].sudo().search([
-                ('product_id', '=',prod_id),
-                ('remaining_qty', '<', 0),
-                ('stock_move_id', '!=', False),
-                ('company_id', '=', company.id),
-            ], order='create_date, id')
-
-            for svl in svls_to_fix:
-                new_vals = {
+            if groups!=[]:
+                unit_cost = product.standard_price
+                value_at_prod = round(product.actual_quantity * product.standard_price,2)
+                vals = {
+                    'product_id': prod_id,
+                    'unit_cost': unit_cost,
+                    'quantity': 0,
+                    'value': 0,
                     'remaining_qty': 0,
-                    'remaining_value': 0,
+                    'company_id': company.id,
+                    'description': 'SPS fix stock valuation layer',
                 }
-                svl.write(new_vals)
+                if quantity != product.actual_quantity:
+                    new_quantity = product.actual_quantity - quantity
+                    vals["quantity"] = new_quantity
 
+                if value != value_at_prod:
+                    new_value = value_at_prod - value
+                    vals["value"] = new_value
+
+                if (quantity != product.actual_quantity) or (value != value_at_prod):
+                    count_of_prod = count_of_prod +1
+                    #adjustment_value = self.env['stock.valuation.layer'].sudo().create(vals)
+
+                svls_to_fix = self.env['stock.valuation.layer'].sudo().search([
+                    ('product_id', '=',prod_id),
+                    ('remaining_qty', '<', 0),
+                    ('stock_move_id', '!=', False),
+                    ('company_id', '=', company.id),
+                ], order='create_date, id')
+
+                for svl in svls_to_fix:
+                    new_vals = {
+                        'remaining_qty': 0,
+                        'remaining_value': 0,
+                    }
+                    rows_corrected = rows_corrected+1
+                    #svl.write(new_vals)
+
+        print("--- %s seconds ---" % (time.time() - start_time))
+        print(count_of_prod)
+        print(rows_corrected)
