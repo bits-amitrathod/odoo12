22f29a40d948eb9d22fa1c1da4bc31bd9a69ff86
Anvay walujkar <anvaywalujkar@benchmarkit.solutions>
2022-02-07 15:28:32 +0530

    new bonus export query fix

--- reports/new_account_bonus_report/controllers/controllers.py
+++ reports/new_account_bonus_report/controllers/controllers.py
@@ -92,7 +92,7 @@ class ExportNewAccountBonusReport(http.Controller):
                         ai.invoice_date                     AS invoice_date, 
                         CASE WHEN so.invoice_status = 'invoiced' then 'Fully Invoiced' END AS invoice_status,
                         CASE WHEN ai.state = 'posted' then 'Posted' END AS invoice_state,
-                        ai.amount_total                     AS amount_total, 
+                          SUM(SOL.qty_delivered * SOL.price_reduce)                      AS amount_total, 
                         X.months                            AS months,
                         ai.currency_id                      AS currency_id,
                         X.first_occurence                   AS date_of_first_order
@@ -140,6 +140,13 @@ class ExportNewAccountBonusReport(http.Controller):
                         public.res_users russ ON rp.account_manager_cust = russ.id
                     LEFT OUTER JOIN 
                         public.res_partner rpss ON russ.partner_id = rpss.id
+                    INNER JOIN public.sale_order_line SOL ON so.id = SOL.order_id 
+                    INNER JOIN 
+                    (SELECT DISTINCT ON (origin) origin,date_done,sale_id  FROM stock_picking WHERE picking_type_id = 5 
+                    AND state = 'done' ORDER BY origin) AS SPS 
+                    ON so.id = SPS.sale_id
+                    INNER JOIN  public.product_product  pp on SOL.product_id = pp.id 
+                    INNER JOIN  public.product_template  pt on pp.product_tmpl_id = pt.id and pt.type!='service'
                 WHERE so.invoice_status = 'invoiced'                
                    """
 
@@ -148,6 +155,12 @@ class ExportNewAccountBonusReport(http.Controller):
 
         if key_account_id != "none":
             select_query = select_query + " AND rp.account_manager_cust = '" + str(key_account_id) + "'"
+
+        group_by = """ GROUP BY so.id,rp.name, rpp.name, rppp.name ,rps.name  ,rpss.name 
+        ,rp.user_id, 
+         X.months  ,X.first_occurence ,ai.invoice_date,ai.state,ai.currency_id  """
+
+        select_query = select_query + group_by
 
         order_by = " ORDER BY rp.name"
 
