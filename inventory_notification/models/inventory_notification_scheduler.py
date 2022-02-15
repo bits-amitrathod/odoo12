2c68106a16ca1baa2fa00ced46f9819c9cdc76c9
Anvay walujkar <anvaywalujkar@benchmarkit.solutions>
2022-02-15 14:13:02 +0530

    email notification button css chnages new

--- inventory_notification/models/inventory_notification_scheduler.py
+++ inventory_notification/models/inventory_notification_scheduler.py
@@ -486,7 +486,7 @@ class InventoryNotificationScheduler(models.TransientModel):
                                     </tbody>
                                     </table>
                                     <div class="text-center" style="text-align: center;">
-                                        <a target="_blank" href="/shop/quote_my_report/""" + str(customr.id) + """" style="background-color:#1abc9c; padding:15px; text-decoration:none; color:#fff; border-radius:5px; font-size:25px; box-shadow: 6px 5px #808080;" class="o_default_snippet_text">BUY NOW</a>
+                                        <a target="_blank" href="/shop/quote_my_report/""" + str(customr.id) + """" style="background-color:#1abc9c; padding:15px 60px 15px 60px; text-decoration:none; color:#fff; border-radius:5px; font-size:25px; box-shadow: 0 8px 16px 0 #a29c9c, 0 6px 20px 0 #b2b0b0;" class="o_default_snippet_text">BUY NOW</a>
                                     </div>
 
                                     """
