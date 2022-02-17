f9e276deea15c7f62f3444b0a974c453ba8d1f8e
Anvay walujkar <anvaywalujkar@benchmarkit.solutions>
2022-02-17 15:22:55 +0530

    notification email button css chnages

--- inventory_notification/models/inventory_notification_scheduler.py
+++ inventory_notification/models/inventory_notification_scheduler.py
@@ -436,10 +436,10 @@ class InventoryNotificationScheduler(models.TransientModel):
                                                                       "<br/> <br/> Below are items you have previously requested that are currently in stock. " \
                                                                       "In addition, below is the link to download full product catalog. Please let us know what" \
                                                                       " ordering needs we can help provide savings on this week! <br/> <a href='https://www.shopsps.com/downloadCatalog'>Click Here to Download SPS Product Catalog </a>" \
-                                                                      """<center>
+                                                                      """<br/><center>
                                                                                 <a target="_blank" href="/shop/quote_my_report/""" + str(
                     customr.id) + """" style="background-color:#1abc9c; padding:15px 60px 15px 60px; text-decoration:none; color:#fff; border-radius:5px; font-size:25px; box-shadow: 0 8px 16px 0 #a29c9c, 0 6px 20px 0 #b2b0b0; " class="o_default_snippet_text">BUY NOW</a>
-                                                                        </center>"""
+                                                                        </center><br/><br/>"""
                 header = ['Manufacturer', 'Catalog number', 'Description', 'Sales Price', 'Quantity On Hand',
                           'Min Exp. Date',
                           'Max Exp. Date', 'Unit Of Measure']
@@ -485,6 +485,7 @@ class InventoryNotificationScheduler(models.TransientModel):
                                     </tr>
                                     </tbody>
                                     </table>
+                                    <br/>
                                     <div class="text-center" style="text-align: center;">
                                         <a target="_blank" href="/shop/quote_my_report/""" + str(customr.id) + """" style="background-color:#1abc9c; padding:15px 60px 15px 60px; text-decoration:none; color:#fff; border-radius:5px; font-size:25px; box-shadow: 0 8px 16px 0 #a29c9c, 0 6px 20px 0 #b2b0b0;" class="o_default_snippet_text">BUY NOW</a>
                                     </div>
