6accb609b1ad94b44766581489700763d0d4df8c
Sarah Egendoerfer <sarah.egendoerfer@captivea.us>
2022-03-30 09:15:25 -0400

    [ADD} Changes 855 (rejected qty) and 856 (UOM)

---
+++ customer_credit_note/captivea_edi/models/__init__.py
@@ -0,0 +1,16 @@
+from . import customer_uom_conf
+from . import product
+from . import res_partner
+from . import setu_edi_export
+from . import setu_edi_log
+from . import stock_picking
+from . import delivery_carrier
+from . import account_move
+from . import sale_order
+from . import sale_order_line
+from . import setu_sftp
+from . import captivea_edi
+from . import remove_log_conf
+from . import ir_cron
+
+
