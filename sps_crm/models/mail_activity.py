9b5ef96a538756be48bbbe9c5c16649b522307ed
Anvay walujkar <anvaywalujkar@benchmarkit.solutions>
2022-07-29 14:28:56 +0530

    activity feedback chnages

--- sps_crm/models/mail_activity.py
+++ sps_crm/models/mail_activity.py
@@ -17,8 +17,30 @@ class MailActivityNotesCustom(models.Model):
         ('medium', 'Medium'),
         ('low', 'Low')], string='Priority', store=True)
 
+    reference = fields.Reference(string='Related Document',
+                                 selection='_reference_models', default='res.partner,1')
+
+    @api.model
+    def _reference_models(self):
+
+        # models = self.env['ir.model'].sudo().search([('state', '!=', 'manual'), ('name', 'not in', model_list),
+        #                                              ('name', 'not like', 'report'), ('name', 'not like', 'popup'),
+        #                                              ('name', 'not like', 'Test'), ('name', 'not like', 'Tests'),
+        #                                              ('name', 'not like', 'sps'), ('name', 'not like', 'prioritization'),
+        #                                              ('name', 'not like', 'Tests')])
+
+        model_list = ['Contact', 'Sales Order', 'Vendor Offer Automation', 'Lead/Opportunity']
+        models = self.env['ir.model'].sudo().search([('state', '!=', 'manual'), ('name', 'in', model_list)])
+        res = [(model.model, model.name)
+               for model in models
+               if not model.model.startswith('ir.')]
+        return res
+
+    @api.depends('acq_activity_notes', 'sales_activity_notes')
     def _compute_act_note_field(self):
         for record in self:
+            # record.res_model = 'res.partner'
+            # record.reference = 'res.partner,1'
             if record.related_partner_activity:
                 partner_link = self.env['partner.link.tracker'].search([('partner_id', '=',
                                                                          record.related_partner_activity.id)], limit=1)
@@ -49,4 +71,11 @@ class MailActivityNotesCustom(models.Model):
                 if partner_link:
                     record.sales_activity_notes = partner_link.sales_activity_notes
                     record.acq_activity_notes = partner_link.acq_activity_notes
+
+    # @api.model
+    # def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+    #     res = super(MailActivityNotesCustom, self).fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar,
+    #                                                submenu=submenu)
+    #     res.res_model = 'res.partner'
+    #     return res
 
