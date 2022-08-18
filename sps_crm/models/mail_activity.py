fd401d00861a85f3e4a1623d857c590c656de8dd
Anvay walujkar <anvaywalujkar@benchmarkit.solutions>
2022-08-18 17:07:45 +0530

    ticket 488

--- sps_crm/models/mail_activity.py
+++ sps_crm/models/mail_activity.py
@@ -66,6 +66,7 @@ class MailActivityNotesCustom(models.Model):
     def onchange_contact(self):
         for record in self:
             if record.related_partner_activity:
+                record.res_name = record.related_partner_activity.name
                 partner_link = self.env['partner.link.tracker'].search([('partner_id', '=',
                                                                          record.related_partner_activity.id)], limit=1)
                 if partner_link:
