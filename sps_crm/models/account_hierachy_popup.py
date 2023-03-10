3ce91d5a8f72b4d055b59a87f32c63e4eb5213fa
Tushar Godase <tushargodase@benchmarkit.solutions>
2023-03-10 14:33:18 +0530

    - replace with space

--- sps_crm/models/account_hierachy_popup.py
+++ sps_crm/models/account_hierachy_popup.py
@@ -124,6 +124,8 @@ class AccountHierarchyReport(models.TransientModel):
     def recursive_hir(self, child, list_all, level, final_data, final_data_name):
         data_dash = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ' * level + '&nbsp;'
         final_data.append(data_dash + '&nbsp;' + child)
+        # data_dash = '------ ' * level + '>'
+        # final_data.append(data_dash + ' ' + child)
         final_data_name.append(child)
         level = level + 1
         if child in list_all and level <= 9:
