3d439a8f665bbd5756fe71c61e0edfc3d16a2cf1
Anvay <anvaywalujkar@benchmarkit.solutions>
2023-03-24 17:21:21 +0530

    590 fix

--- website_sales/models/temp_product_list.py
+++ website_sales/models/temp_product_list.py
@@ -156,6 +156,128 @@ class TempProductList(models.Model):
                          )''' % (self._table)
                                 )
 
+    def set_empty_product_list(self, partner_id=None):
+        #self.product_list.clear()
+        temp_list = {}
+        partner_id = self.env.context.get('quote_my_report_partner_id')
+        if partner_id and partner_id is not None:
+            #partner = self.env['res.partner'].search([('id', '=', partner_id), ])
+            #parent_partner_id = partner.id
+            partner_list = []
+            #while not partner.is_parent:
+            parent_partner_id = self.get_parent(partner_id)
+            partner_list.append(parent_partner_id)
+            #print(self.env['quotation.product.list'].search([('partner', '=', parent_partner_id)]))
+            if self.product_list.get(parent_partner_id) and self.product_list.get(parent_partner_id) is not None:
+                # print(self.product_list.get(parent_partner_id))
+                self.product_list.pop(parent_partner_id)
+            self.env.cr.execute("select id from res_partner where parent_id =" + str(parent_partner_id).replace(",)", ")"))
+            chil_list = self.env.cr.dictfetchall()
+            for i in chil_list:
+                partner_list.append(i['id'])
+            sql_query = """
+                        SELECT  DISTINCT on (partn_name)
+                        CONCAT(sale_order.partner_id, a.id) as partn_name,
+                        ROW_NUMBER () OVER (ORDER BY sale_order.partner_id) as id,
+                        sale_order.partner_id AS partner_id,
+                        a.id AS product_id,
+                        a.product_brand_id AS product_brand_id,      
+                        null as min_expiration_date,
+                        null as max_expiration_date,                  
+                        1 as quantity                                     
+                        FROM
+                        sale_order
+                        INNER JOIN sale_order_line
+                        ON(
+                        sale_order.id = sale_order_line.order_id and sale_order.partner_id in """+str(tuple(partner_list)).replace(",)", ")")+""")
+                        INNER JOIN (select product_product.id , product_template.product_brand_id 
+                                from product_product
+                                INNER JOIN product_template
+                                ON(product_product.product_tmpl_id = product_template.id  and
+                                     product_template.actual_quantity > 0 and
+                                     product_template.sale_ok = True and product_template.is_published = True and
+                                     product_template.active = True)
+                                Where product_product.active = True and product_product.id NOT IN (Select product_id 
+                                from public.exclude_product_in_stock where partner_id = """ + str(parent_partner_id) + """)) as a         
+                        ON(sale_order_line.product_id = a.id)
+                        """
+            groupby = """ 
+
+                     group by partn_name, public.sale_order.partner_id,
+                           a.id,
+                           a.product_brand_id
+                            """
+
+            sql_query = sql_query + groupby
+
+            self.env.cr.execute(sql_query)
+            query_results = self.env.cr.dictfetchall()
+            partner = self.env['res.partner'].search([('id', '=', parent_partner_id)])
+            for query_result in query_results:
+                product = self.env['product.product'].search([('id', '=', query_result['product_id'])])
+                product_brand = self.env['product.brand'].search([('id', '=', query_result['product_brand_id'])])
+
+                # get product expiration date
+                self.env.cr.execute(
+                    """
+                    SELECT
+                    sum(quantity), min(use_date), max(use_date)
+                FROM
+                    stock_quant
+                INNER JOIN
+                    stock_production_lot
+                ON
+                    (
+                        stock_quant.lot_id = stock_production_lot.id)
+                INNER JOIN
+                    stock_location
+                ON
+                    (
+                        stock_quant.location_id = stock_location.id)
+                WHERE
+                    stock_location.usage in('internal', 'transit') and stock_production_lot.product_id  = %s
+                    """,
+                    (query_result['product_id'],))
+                result = self.env.cr.dictfetchone()
+
+                if partner.property_product_pricelist.id and product:
+                    price_list_without_round_off = partner.property_product_pricelist.get_product_price(
+                        product, product.product_tmpl_id.actual_quantity, partner)
+                    #price_list = float("{0:.2f}".format(price_list_without_round_off))
+                    price_list = round(price_list_without_round_off,2)
+                else:
+                    if product.product_tmpl_id.list_price:
+                        price_list = float("{0:.2f}".format(product.product_tmpl_id.list_price))
+                    else:
+                        price_list = product.product_tmpl_id.list_price
+
+                company_fetch = self.env['res.company'].search([], limit=1, order="id desc")
+                product_dict = {'product': product,
+                                'partner': partner,
+                                'partn_name': query_result['partn_name'],
+                                'product_brand_name': product_brand.name,
+                                'product_sku': product.product_tmpl_id.sku_code,
+                                'min_expiration_date': result['min'],
+                                'max_expiration_date': result['max'],
+                                'price_list': price_list,
+                                'price_curr': company_fetch.currency_id,
+                                'quantity': query_result['quantity'],
+                                'select': False}
+
+                product_data = {product.id: product_dict}
+                temp_list.update(product_data)
+            customer_data={partner.id:temp_list}
+            self.product_list.update(customer_data)
+        else:
+            # This Code For only console error resolve purposr
+            self.env.cr.execute('''
+                         CREATE OR REPLACE VIEW %s AS (
+                         SELECT  so.id AS id,
+                                 so.name AS name
+                         FROM sale_order so
+                         )''' % (self._table)
+                                )
+
     #  @api.model_cr
     def delete_and_create(self):
         self.init_table()
@@ -186,6 +308,8 @@ class TempProductList(models.Model):
         _logger.info('- update_quote_my_report_json_list  partner_id  : %s', partner_id)
         _logger.info('- update_quote_my_report_json_list  parent_partner_id  : %s', parent_partner_id)
         _logger.info('- update_quote_my_report_json_list  self.product_list  : %s', self.product_list)
+        if parent_partner_id and parent_partner_id is not None and len(self.product_list) == 0:
+            self.set_empty_product_list(parent_partner_id)
         partner_product_list = self.product_list.get(parent_partner_id)
         _logger.info('- update_quote_my_report_json_list  partner_product_list  : %s', partner_product_list)
         if partner_product_list:
@@ -208,6 +332,8 @@ class TempProductList(models.Model):
     def get_product_list(self, partner_id):
         _logger.info('update_quote_my_report_json_list In get_product_list -  partner_id : %s', str(partner_id))
         try:
+            if partner_id and partner_id is not None and len(self.product_list) == 0:
+                self.init_table()
             if partner_id and partner_id is not None and len(self.product_list) > 0:
                 parent_id = self.get_parent(partner_id)
                 _logger.info('update_quote_my_report_json_list In get_product_list - parent_id : %s', str(parent_id))
