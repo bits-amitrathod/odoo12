a9579c8e357005ffa18bbdb44a897f942c0f890d
Tushar Godase <tushargodase@benchmarkit.solutions>
2023-06-22 14:07:36 +0530

    two fiels added

--- vendor_offer/models/purchase_order_app.py
+++ vendor_offer/models/purchase_order_app.py
@@ -28,10 +28,13 @@ class VendorOfferNewAppraisal(models.Model):
     no_match_sku_import = fields.Text(string='SKU Cleaned', readonly=True)
     no_match_sku_import_cleaned = fields.Text(string='SKU', readonly=True)
 
+    is_change_tier1_to_premium = fields.Boolean(string="Change Items Priced as Tier 1 to Premium")
+    is_dynamic_tier_adjustment = fields.Boolean(string="Allow Dynamic Tier Adjustment?", default=True)
+
     # This Method Convert cancelled PO -> Vendor Offer
     def button_vendor_offer(self):
         _logger.info("Set to VO button Action..")
-        self.write({'state': 'ven_draft'})
+        self.write({'state': 'ven_draft', 'status': 'ven_draft', 'status_ven': ''})
         self.action_recalculate_vendor_offer()
         return {}
 
@@ -41,8 +44,10 @@ class VendorOfferNewAppraisal(models.Model):
             for obj in objList:
                 for obj_line in obj.order_line:
                     obj_line.set_values()
+                    if obj.is_change_tier1_to_premium:
+                        pass  # TODO : Tier 1 products multiplier increased to Premium
                     if obj_line.is_recalculate_multiplier():
-                        obj_line.multiplier_adjustment_criteria()
+                        obj_line.multiplier_adjustment_criteria() if obj.is_dynamic_tier_adjustment else obj_line.no_tier_multiplier_adjustment_criteria()
                     obj_line.copy_product_qty_column()
                     obj_line._cal_offer_price()
                     obj_line._set_offer_price()
