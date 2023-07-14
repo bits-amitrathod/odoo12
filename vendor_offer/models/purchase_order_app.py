e035d85de35740dd9cb4b7731c2e294e6c2f5559
Tushar Godase <tushargodase@benchmarkit.solutions>
2023-07-14 16:33:56 +0530

    manual import re cal button added

--- vendor_offer/models/purchase_order_app.py
+++ vendor_offer/models/purchase_order_app.py
@@ -64,7 +64,7 @@ class VendorOfferNewAppraisal(models.Model):
                     obj_line._cal_margin()
                     obj_line.compute_total_line_vendor()
                     obj_line.compute_average_retail()
-                    # obj.summary_calculate(obj_line)
+                    obj.summary_calculate()
 
     def action_manual_recalculate_vendor_offer(self):
         for objList in self:
@@ -90,7 +90,7 @@ class VendorOfferNewAppraisal(models.Model):
                     obj_line._cal_margin()
                     obj_line.compute_total_line_vendor()
                     obj_line.compute_average_retail()
-                    # obj.summary_calculate()
+                    obj.summary_calculate()
 
     def summary_calculate(self):
         for po in self:
