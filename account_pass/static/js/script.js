0c18018327adfcd1aca641349f6890b0ea87b74b
Tushar Godase <tushargodase@benchmarkit.solutions>
2023-08-22 11:18:50 +0530

    js changes

--- account_pass/static/js/script.js
+++ account_pass/static/js/script.js
@@ -2,9 +2,10 @@ odoo.define('account_pass.pass_jquery', function (require) {
     'use strict';
 
     var rpc = require('web.rpc');
-
     $(window).on('hashchange', function() {
-        console.log('Hey .............. 1')
-      $('.abc').closest('td').closest('tr').find('td:first').find('label:first').css("text-decoration", "underline");
+        var url_loader = window.location.href;
+        if (url_loader.includes('model=account.pass&view_type=form')) {
+            $('.label_underline').closest('td').closest('tr').find('td:first').find('label:first').css("border-bottom", "2px solid");
+        }
        });
 });
