836d85935cc58b7554a8a5ebb4ffc87a9199b942
Anvay <anvaywalujkar@benchmarkit.solutions>
2023-09-27 16:43:23 +0530

    675 fix

--- vendor_bill_partner_name/models/models.py
+++ vendor_bill_partner_name/models/models.py
@@ -6,7 +6,7 @@ from odoo import api, fields, models, tools, SUPERUSER_ID, _
 from odoo.exceptions import UserError, ValidationError
 from odoo.modules import get_module_resource
 from dateutil.relativedelta import relativedelta
-from odoo.tools.misc import format_date
+from odoo.tools.misc import format_date,get_lang
 MAP_INVOICE_TYPE_PARTNER_TYPE = {
     'out_invoice': 'customer',
     'out_refund': 'customer',
@@ -315,4 +315,45 @@ class AccountMoveVendorBill(models.Model):
                     format_date(rec.env, rec.invoice_date_due, date_format="MM/dd/yyyy"))
             else:
                 rec.due_date_note = due_str
+
+    def action_invoice_sent(self):
+        """ Open a window to compose an email, with the edi invoice template
+            message loaded by default
+        """
+        self.ensure_one()
+        template = self.env.ref('vendor_bill_partner_name.email_template_edi_invoice_custom', raise_if_not_found=False)
+        lang = False
+        if template:
+            lang = template._render_lang(self.ids)[self.id]
+        if not lang:
+            lang = get_lang(self.env).code
+        compose_form = self.env.ref('account.account_invoice_send_wizard_form', raise_if_not_found=False)
+        email_from = 'accounting@shopsps.com'
+        ctx = dict(
+            default_model='account.move',
+            default_res_id=self.id,
+            # For the sake of consistency we need a default_res_model if
+            # default_res_id is set. Not renaming default_model as it can
+            # create many side-effects.
+            default_res_model='account.move',
+            default_use_template=bool(template),
+            default_template_id=template and template.id or False,
+            default_composition_mode='comment',
+            mark_invoice_as_sent=True,
+            custom_layout="mail.mail_notification_paynow",
+            model_description=self.with_context(lang=lang).type_name,
+            force_email=True,
+            email_from=email_from
+        )
+        return {
+            'name': _('Send Invoice'),
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'account.invoice.send',
+            'views': [(compose_form.id, 'form')],
+            'view_id': compose_form.id,
+            'target': 'new',
+            'context': ctx,
+        }
 
