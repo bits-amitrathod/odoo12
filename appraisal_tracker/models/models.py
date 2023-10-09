ad40e911e03419cea8f6484d5e5b82983ce0979b
Anvay <anvaywalujkar@benchmarkit.solutions>
2023-10-09 11:55:27 +0530

    678 fix account changed to 50007

--- appraisal_tracker/models/models.py
+++ appraisal_tracker/models/models.py
@@ -254,11 +254,13 @@ class apprisal_tracker_vendor(models.Model):
         self.ensure_one()
         aml_currency = move and move.currency_id or self.currency_id
         date = move and move.date or fields.Date.today()
+        account_id = self.env['account.account'].search([('code', '=', '50007')], limit=1)
         res = {
             'sequence': 1,
             'name': 'CREDIT (%s - %s)' % (self.name, self.appraisal_no),
             'quantity': 1,
-            'price_unit': credit_amount
+            'price_unit': credit_amount,
+            'account_id': account_id.id
         }
         if not move:
             return res
@@ -272,8 +274,7 @@ class apprisal_tracker_vendor(models.Model):
             'move_id': move.id,
             'currency_id': currency and currency.id or False,
             'date_maturity': move.invoice_date_due,
-            'partner_id': move.partner_id.id,
-            'name': 'CREDIT (%s - %s)' % (self.name, self.appraisal_no)
+            'partner_id': move.partner_id.id
         })
         return res
 
