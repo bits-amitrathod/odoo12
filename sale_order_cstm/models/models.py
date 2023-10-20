c4d569adb49365e84b37d856a241fc0b4e9a43a0
Tushar Godase <tushargodase@benchmarkit.solutions>
2023-10-20 12:37:06 +0530

    sub total cal changed

--- sale_order_cstm/models/models.py
+++ sale_order_cstm/models/models.py
@@ -32,6 +32,23 @@ class SaleOrderAvailability(models.Model):
                 return {'warning': warning_mess}
         return {}
 
+    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
+    def _compute_amount(self):
+        """
+        Compute the amounts of the SO line.
+        """
+        for line in self:
+            price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty,
+                                            product=line.product_id, partner=line.order_id.partner_shipping_id)
+            price2 = line.price_reduce * (1 - (line.discount or 0.0) / 100.0)
+            price_subtotal = line.product_uom_qty * price2
+            line.update({
+                'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
+                'price_total': taxes['total_included'],
+                'price_subtotal': price_subtotal,
+            })
+
 class SaleOrderCstm(models.Model):
     _inherit = "sale.order"
 
