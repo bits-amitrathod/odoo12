52701ecdef858914b47b8767f3c4344124b8401e
Anvay <anvaywalujkar@benchmarkit.solutions>
2024-04-25 15:01:05 +0530

    pre migrate for some fixes

--- prioritization_engine/migrations/16.2/pre-migrate.py
+++ prioritization_engine/migrations/16.2/pre-migrate.py
@@ -20,22 +20,14 @@ def migrate(cr, version):
         where id in (1930)
         and name = 'report_invoice_document_extended' limit 1)
     """)
-    cr.execute("""
-            update ir_ui_view set active = true where name = 'purchase.order.form' and id = 1953
-        """)
-    cr.execute("""
-               ALTER TABLE payment_transaction DROP CONSTRAINT payment_transaction_acquirer_id_fkey;
-            """)
-    cr.execute("""
-                DELETE FROM ir_asset where id=94;
-            """)
+    cr.execute("""update ir_ui_view set active = true where name = 'purchase.order.form' and id = 1953""")
+    cr.execute("""DELETE FROM ir_ui_view where inherit_id in (6176, 5895, 5698, 5697, 1953, 5696,4216,7559);""")
+    cr.execute("""DELETE FROM ir_ui_view where id in (5742,5696,4216,4901);""")
+    cr.execute("""DELETE FROM ir_cron where ir_actions_server_id in (2064, 1902);""")
+    cr.execute("""ALTER TABLE payment_transaction DROP CONSTRAINT payment_transaction_acquirer_id_fkey;""")
+    cr.execute("""DELETE FROM ir_asset where id=94;""")
+    cr.execute("""DELETE FROM mail_template WHERE id in (13, 14, 134);""")
+    cr.execute("""DELETE FROM ir_model_data WHERE name in ('email_template_edi_purchase', 'email_template_edi_purchase_done', 'email_template_edi_purchase_reminder')""")
 
-    cr.execute("""
-                DELETE FROM ir_ui_view where inherit_id in (6176, 5895, 5698, 5697, 1953, 5696,4216,7559,4044);
-            """)
-
-    cr.execute("""
-                DELETE FROM ir_ui_view where id in (5742,5696,4216,4044);
-            """)
 
     _logger.info(" ------- Pri-Migration Ended -------" + "\n" * 20)
