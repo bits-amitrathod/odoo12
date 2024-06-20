eb9e3569492188bc43d18c3092c30629427727e0
Anvay <anvaywalujkar@benchmarkit.solutions>
2024-06-20 12:41:15 +0530

    1052 changes

--- vendor_offer/models/vendor_offer_expired.py
+++ vendor_offer/models/vendor_offer_expired.py
@@ -11,7 +11,7 @@ class VendorOfferExpired(models.Model):
     offer_approved = fields.Boolean(string='Offer is Approved', track_visibility='onchange')
 
     def set_expiration_flag_old_offer(self):
-        fourteen_days_ago = datetime.datetime.today().date() - datetime.timedelta(days=14)
+        fourteen_days_ago = datetime.datetime.today().date() - datetime.timedelta(days=21)
 
         expired_offers = self.env['purchase.order'].search([
             ('date_offered', '<', fourteen_days_ago),
