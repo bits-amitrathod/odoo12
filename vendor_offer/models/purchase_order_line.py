faa2f4be1f3828af193dc0cc0096525ac54ede88
bits-aman <ahujaaman@benchmarkit.solutions>
2024-07-24 15:42:56 +0530

    [ADD] appraisal points have been added

--- vendor_offer/models/purchase_order_line.py
+++ vendor_offer/models/purchase_order_line.py
@@ -57,23 +57,22 @@ class VendorOfferProduct(models.Model):
                                                     compute="_calculat_bill_price")
     billed_product_retail_price = fields.Monetary("Total Billed Qty Retail Price", store=False,
                                                      compute="_calculat_bill_price")
+    do_not_change_retail = fields.Boolean(string="Do Not Change retail", default=False)
+    do_not_change_offer = fields.Boolean(string="Do Not Change offer", default=False)
 
-
-
-    #@api.multi
+    # @api.multi
     def _calculat_delv_price(self):
         for order in self:
             for p in order:
                 order.delivered_product_offer_price = round(p.qty_received * p.product_offer_price, 2)
                 order.delivered_product_retail_price = round(p.qty_received * p.product_unit_price, 2)
 
-    #@api.multi
+    # @api.multi
     def _calculat_bill_price(self):
         for order in self:
             for p in order:
                 order.billed_product_offer_price = round(p.qty_invoiced * p.product_offer_price, 2)
                 order.billed_product_retail_price = round(p.qty_invoiced * p.product_unit_price, 2)
-
 
     def action_show_details(self):
         multi = self.env['stock.move'].search([('purchase_line_id', '=', self.id)])
@@ -85,7 +84,41 @@ class VendorOfferProduct(models.Model):
     def set_values_from_import(self):
         pass
 
-    @api.onchange('product_id')
+    def calculate_order_line_product_values(self):
+        obj = self.order_id
+        for obj_line in self:
+            # if obj_line.import_type_ven_line != 'new_appraisal':
+            obj_line.set_values()
+            obj_line.compute_new_fields_vendor_line()
+            obj_line.set_default_multiplier()
+            if obj.is_change_tier1_to_premium:
+                obj_line.upgrade_multiplier_tier1_to_premium()
+            if obj_line.dont_recalculate_offer_price is not True:
+                obj_line.multiplier_adjustment_criteria() if obj.is_dynamic_tier_adjustment else obj_line.no_tier_multiplier_adjustment_criteria()
+                obj_line.overstock_threshold()
+            obj_line.copy_product_qty_column()
+            obj_line._cal_offer_price()
+            obj_line._set_offer_price()
+            obj_line._cal_margin()
+            obj_line.compute_total_line_vendor()
+            obj_line.compute_average_retail()
+        obj.summary_calculate()
+
+    @api.onchange('multiplier')
+    def onchange_order_line_multiplier(self):
+        for line in self:
+            if line.id.origin:
+                line.dont_recalculate_offer_price = True
+    @api.onchange('product_unit_price')
+    def onchange_order_line_product_unit_price(self):
+        for line in self:
+            if line.id.origin:
+                line.do_not_change_retail = True
+    @api.onchange('product_offer_price')
+    def onchange_order_line_product_offer_price(self):
+        for line in self:
+            if line.id.origin:
+                line.do_not_change_offer = True
     @api.depends('product_id')
     def onchange_product_id_vendor_offer(self):
         for line in self:
@@ -93,91 +126,16 @@ class VendorOfferProduct(models.Model):
 
             line.sku_code = line.product_id.product_tmpl_id.sku_code
             line.product_brand_id = line.product_id.product_tmpl_id.product_brand_id
-            # total = total_m = total_90 = total_yr = 0
-            # str_query_cm = "SELECT sum(sml.qty_done) FROM sale_order_line AS sol LEFT JOIN stock_picking AS sp ON " \
-            #                "sp.sale_id=sol.id " \
-            #                " LEFT JOIN stock_move_line AS sml ON sml.picking_id=sp.id WHERE sml.state='done' AND " \
-            #                "sml.location_dest_id =%s AND" \
-            #                " sml.product_id =%s"
-            # cust_location_id = self.env['stock.location'].search([('name', '=', 'Customers')]).id
-            # today_date = datetime.datetime.now()
-            # last_month = fields.Date.to_string(today_date - datetime.timedelta(days=30))
-
             if line.env.context.get('vendor_offer_data') or line.state == 'ven_draft' or line.state == 'ven_sent':
                 result1 = {}
                 if not line.product_id:
                     return result1
                 if line.product_qty_app_new is False or line.product_qty_app_new == 0.0:
                     line.product_qty_app_new = 1
-                #if line.order_id.import_type_ven != 'new_appraisal':
-
                 ''' sale count will show only done qty '''
-
-                # last_3_months = fields.Date.to_string(today_date - datetime.timedelta(days=90))
-                # last_yr = fields.Date.to_string(today_date - datetime.timedelta(days=365))
-                #
-                # self.env.cr.execute(str_query_cm + " AND sp.date_done>=%s", (cust_location_id,
-                #                                                              line.product_id.id, last_month))
-                # quant_m = self.env.cr.fetchone()
-                # if quant_m[0] is not None:
-                #     total_m = total_m + int(quant_m[0])
-                # line.product_sales_count_month = total_m
-                #
-                # self.env.cr.execute(str_query_cm + " AND sp.date_done>=%s", (cust_location_id,
-                #                                                              line.product_id.id, last_3_months))
-                # quant_90 = self.env.cr.fetchone()
-                # if quant_90[0] is not None:
-                #     total_90 = total_90 + int(quant_90[0])
-                # line.product_sales_count_90 = total_90
-                #
-                # self.env.cr.execute(str_query_cm + " AND sp.date_done>=%s", (cust_location_id,
-                #                                                              line.product_id.id, last_yr))
-                # quant_yr = self.env.cr.fetchone()
-                # if quant_yr[0] is not None:
-                #     total_yr = total_yr + int(quant_yr[0])
-                # line.product_sales_count_yrs = total_yr
-                #
-                # self.env.cr.execute(str_query_cm, (cust_location_id, line.product_id.id))
-                # quant_all = self.env.cr.fetchone()
-                # if quant_all[0] is not None:
-                #     total = total + int(quant_all[0])
-                # line.product_sales_count = total
-
                 line.qty_in_stock = line.product_id.actual_quantity
-                if line.order_id.import_type_ven != 'new_appraisal':
-                    if line.order_id.is_dynamic_tier_adjustment:
-                        line.multiplier_adjustment_criteria()
-                    else:
-                        line.no_tier_multiplier_adjustment_criteria()
-
-                # if line.multiplier.id == False:
-                #     if line.product_tier.code == False:
-                #         multiplier_list = line.env['multiplier.multiplier'].search([('code', '=', 'out of scope')])
-                #         line.multiplier = multiplier_list.id
-                #     elif line.product_sales_count == 0:
-                #         multiplier_list = line.env['multiplier.multiplier'].search([('code', '=', 'no history')])
-                #         line.multiplier = multiplier_list.id
-                #     elif float(line.qty_in_stock) > (
-                #             line.product_sales_count * 2) and line.product_sales_count != 0:
-                #         multiplier_list = line.env['multiplier.multiplier'].search([('code', '=', 'overstocked')])
-                #         line.multiplier = multiplier_list.id
-                #     elif line.product_id.product_tmpl_id.premium == True:
-                #         multiplier_list = line.env['multiplier.multiplier'].search([('code', '=', 'premium')])
-                #         line.multiplier = multiplier_list.id
-                #     elif line.product_tier.code == '1':
-                #         multiplier_list = line.env['multiplier.multiplier'].search([('code', '=', 't1 good 45')])
-                #         line.multiplier = multiplier_list.id
-                #     elif line.product_tier.code == '2':
-                #         multiplier_list = line.env['multiplier.multiplier'].search([('code', '=', 't2 good 35')])
-                #         line.multiplier = multiplier_list.id
-
-                # line.update_product_expiration_date()
-
                 if (line.product_qty == False):
                     line.product_qty = '1'
-                    # line.price_subtotal = line.list_price ???
-                    # line.product_unit_price = line.list_price
-
                 self.expired_inventory_cal(line)
 
     def expired_inventory_cal(self, line):
@@ -194,37 +152,15 @@ class VendorOfferProduct(models.Model):
     # @api.depends('multiplier', 'order_id.possible_competition')
     def _cal_offer_price(self):
         for line in self:
-            # if line.import_type_ven_line == all_field_import:
-            #     if 'order_list_list' in request.session:
-            #         VendorOfferProduct.set_values_from_import(line)
-            # else:
             multiplier_list = line.multiplier
-            # Added to fix inhirit issue
-
-            # product_unit_price = math.floor(
-            #     round(float(line.product_id.list_price) * (float(multiplier_list.retail) / 100), 2))
 
             val_t = float(line.product_id.list_price) * (float(multiplier_list.retail) / 100)
             if (float(val_t) % 1) >= 0.5:
-                product_unit_price = math.ceil(
-                    float(line.product_id.list_price) * (float(multiplier_list.retail) / 100))
-
+                product_unit_price = math.ceil(float(line.product_id.list_price) * (float(multiplier_list.retail) / 100))
             else:
-                product_unit_price = math.floor(
-                    float(line.product_id.list_price) * (float(multiplier_list.retail) / 100))
-
-            margin = 0
-            if line.multiplier.id:
-                margin += line.multiplier.margin
-
-            if line.possible_competition.id:
-                margin += line.possible_competition.margin
-
-            line.update({
-                'margin': margin
-            })
-
-            if line.dont_recalculate_offer_price is not True:
+                product_unit_price = math.floor(float(line.product_id.list_price) * (float(multiplier_list.retail) / 100))
+            line._cal_margin()
+            if line.do_not_change_retail == False:
                 line.update({
                     'product_unit_price': product_unit_price,
                 })
@@ -250,26 +186,8 @@ class VendorOfferProduct(models.Model):
     # @api.depends('multiplier', 'order_id.possible_competition')
     def _set_offer_price(self):
         for line in self:
-            # if line.import_type_ven_line == all_field_import:
-            #     if 'order_list_list' in request.session:
-            #         VendorOfferProduct.set_values_from_import(line)
-            # else:
             multiplier_list = line.multiplier
-
-            # product_unit_price = math.floor(
-            #     round(float(line.product_id.list_price) * (float(multiplier_list.retail) / 100), 2))
-            # product_offer_price = math.floor(float(product_unit_price) * (
-            #         float(multiplier_list.margin) / 100 + float(line.possible_competition.margin) / 100))
-
-            val_t = float(line.product_id.list_price) * (float(multiplier_list.retail) / 100)
-            if (float(val_t) % 1) >= 0.5:
-                product_unit_price = math.ceil(
-                    float(line.product_id.list_price) * (float(multiplier_list.retail) / 100))
-
-            else:
-                product_unit_price = math.floor(
-                    float(line.product_id.list_price) * (float(multiplier_list.retail) / 100))
-
+            product_unit_price = line.product_unit_price
             val_off = float(product_unit_price) * (float(
                 multiplier_list.margin) / 100 + float(line.possible_competition.margin) / 100)
             if (float(val_off) % 1) >= 0.5:
@@ -277,27 +195,17 @@ class VendorOfferProduct(models.Model):
                     float(product_unit_price) * (
                             float(multiplier_list.margin) / 100 + float(
                         line.possible_competition.margin) / 100))
-
             else:
                 product_offer_price = math.floor(float(product_unit_price) * (
                         float(multiplier_list.margin) / 100 + float(
                     line.possible_competition.margin) / 100))
 
-            if line.dont_recalculate_offer_price is not True:
+            if line.do_not_change_offer == False:
                 line.update({
                     'product_offer_price': product_offer_price
                 })
 
     product_offer_price = fields.Monetary(string="Offer Price", default=_set_offer_price, store=True)
-
-    # def update_product_expiration_date(self):
-    #     for order in self:
-    #         order.env.cr.execute(
-    #             "SELECT min(use_date), max(use_date) FROM public.stock_lot where product_id =" + str(
-    #                 order.product_id.id))
-    #         query_result = order.env.cr.dictfetchone()
-    #         if query_result['max'] != None:
-    #             self.expiration_date = fields.Datetime.from_string(str(query_result['max'])).date()
 
     @api.onchange('product_qty', 'product_offer_price', 'taxes_id')
     @api.depends('product_qty', 'product_offer_price', 'taxes_id')
