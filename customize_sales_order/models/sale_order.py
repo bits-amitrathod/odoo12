d4455cb1f4b7fda375ec4c3513c4c3ca6be49980
bits-aman <ahujaaman@benchmarkit.solutions>
2024-09-02 20:24:22 +0530

    [FIX] customize_sales_order issue fixed

--- customize_sales_order/models/sale_order.py
+++ customize_sales_order/models/sale_order.py
@@ -139,7 +139,8 @@ class sale_order(models.Model):
         # Add note in pick delivery
         if self.state and self.state in 'sale':
             for pick in self.picking_ids:
-                pick.note = val['sale_note'] if ('sale_note' in val.keys()) else self.sale_note
+                if pick.note == False:
+                    pick.note = val['sale_note'] if ('sale_note' in val.keys()) else self.sale_note
 
         if self.carrier_id and self.state and self.state in 'sale':
             stock_pickings = self.env['stock.picking'].search([('sale_id', '=', self.id), ('picking_type_id', '=', 5)])
