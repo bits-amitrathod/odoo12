bfd645620253a546100cecf3ea400ae642fed7fc
Tushar Godase <tushargodase@benchmarkit.solutions>
2024-09-04 12:09:43 +0530

    email template changed

--- prioritization_engine/migrations/16.21/post-migrate.py
+++ prioritization_engine/migrations/16.21/post-migrate.py
@@ -28,7 +28,7 @@ def migrate(cr, version):
     # My Activity -> Filter view activate
     cr.execute("""update ir_ui_view set active = true where id in (5733);""")
     # Accounting -> vendor -> payment
-    cr.execute("""update ir_ui_view set active = true where id in (5217);""")
+    cr.execute("""update ir_ui_view set active = true where id in (5217,5726);""")
     # Client don't want the this view (customer meeting info button)
     cr.execute("""update ir_ui_view set active = false where name = 'res_partner.view.form.calendar';""")
 
