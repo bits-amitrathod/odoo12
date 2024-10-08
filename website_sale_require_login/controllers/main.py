646e1cc8430a2e0d6ddf3d9ad7e51b40bf89f043
Tushar Godase <tushargodase@benchmarkit.solutions>
2024-10-08 15:06:31 +0530

    [fix] sale team update issue resolved

--- website_sale_require_login/controllers/main.py
+++ website_sale_require_login/controllers/main.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import http
+from odoo.http import request
 
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
@@ -9,4 +10,7 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 class RequireLoginToCheckout(WebsiteSale):
     @http.route(auth="user")
     def checkout(self, **post):
-        return super().checkout(**post)
+        response = super().checkout(**post)
+        order = request.website.sale_get_order()
+        order.team_id = order.original_team_id if order else order.team_id
+        return response
