81eb1f0ce7b238471d6429817275fface5c80959
Tushar Godase <tushargodase@gmail.com>
2025-06-25 14:51:46 +0530

    [fix] multi email issue

--- sps_theme/controllers/instock_unsubscribe_controller.py
+++ sps_theme/controllers/instock_unsubscribe_controller.py
@@ -20,22 +20,18 @@ class InStockUnsubscribe(http.Controller):
 
 
         # Get all potential contacts (company and individuals) within the hierarchy
-        # that have an email and are not already ended.
-        # ('end_date', '=', False), ? need to add filter for the checkbox value and display records on screen
-
+        # Those have an email.
+        # In-stock email notification checkboxes are not set true.
         all_potential_contacts = request.env['res.partner'].sudo().search([
             ('id', 'child_of', parent_company.id),
             ('type','=','contact'),
             ('email', '!=', False),
             ('instock_unsubscribe','=', False) ,
-            # ('disable_all_instock_email', '=', False)
+            ('disable_all_instock_email', '=', False)
         ])
-        logger.info('all_potential_contacts %s' ,all_potential_contacts)
 
         if parent_company.email and not parent_company.instock_unsubscribe and not parent_company.disable_all_instock_email:
             all_potential_contacts  |= parent_company  # add to recordset
-
-        logger.info('all_potential_contacts with parent %s' ,all_potential_contacts)
 
         # We create a new list that will only contain one contact per unique email address.
         # This ensures the email is not shown twice on the form.
@@ -45,9 +41,6 @@ class InStockUnsubscribe(http.Controller):
             if contact.email and contact.email not in seen_emails:
                 display_contacts.append(contact)
                 seen_emails.add(contact.email)
-
-        logger.info('display_contacts %s' ,display_contacts)
-
 
         return request.render('sps_theme.instock_unsubscriber_page_template', {
             'contacts': display_contacts,
@@ -60,34 +53,23 @@ class InStockUnsubscribe(http.Controller):
     def unsubscribe_submit(self, **post):
 
         contact_ids_str = request.httprequest.form.getlist('contact_ids')
-
         user_partner = request.env.user.partner_id
         parent_company = user_partner.parent_id or user_partner
-
         feedback = post.get('feedback')
-
         submitted_contacts = request.env['res.partner'].sudo().browse([int(cid) for cid in contact_ids_str])
-        logger.info('submitted_contacts %s',submitted_contacts)
 
         emails_to_unsubscribe = list(set(c.email for c in submitted_contacts if c.email))
-        logger.info('emails_to_unsubscribe %s',emails_to_unsubscribe)
 
         related_contacts = request.env['res.partner'].sudo().search([
             ('id', 'child_of', parent_company.id)
         ])
 
-
         # contacts_to_update = request.env['res.partner'].sudo().search([
         #     ('id', 'child_of', parent_company.id),
         #     # ('email', 'in', emails_to_unsubscribe)
         # ])
-        logger.info('related_contacts %s', related_contacts)
-
-        # logger.info('contacts_to_update %s', contacts_to_update)
-
 
         unsubscribe_all = post.get('unsubscribe_all') == 'on'
-        logger.info('unsubscribe_all %s',unsubscribe_all)
 
         if unsubscribe_all:
             # Set global unsubscribe at company level
@@ -114,18 +96,22 @@ class InStockUnsubscribe(http.Controller):
                     'unsubscribe_feedback': feedback
 
                 })
+
                 # NEW: Also update parent company if email is same and checkbox was checked
                 if is_checked and contact.email and contact.email == parent_company.email:
-                    logger.info('Parent company has same email, updating parent unsubscribed field')
-                    parent_company.sudo().write({
-                        'instock_unsubscribe': True,
-                        'unsubscribe_feedback': feedback
-                    })
-                    contact.sudo().write({
-                        'instock_unsubscribe': is_checked,
-                        'unsubscribe_feedback': feedback
+                    # Find all contacts with the same email (includes parent if it shares the email)
+                    all_same_email = request.env['res.partner'].sudo().search([
+                        ('email', '=', contact.email),
+                        ('id', 'in', related_contacts.ids)
+                    ])
 
-                    })
+                    for record in all_same_email:
+                        logger.info('record %s', record)
+                        record.sudo().write({
+                            'instock_unsubscribe': True,
+                            'unsubscribe_feedback': feedback
+                        })
+
 
         return request.render('sps_theme.instock_unsubscribe_success')
 
