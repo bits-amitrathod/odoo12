c7a9868b968c186ecaea828ebb33ca791e09bc98
Tushar Godase <tushargodase@gmail.com>
2025-12-18 22:38:44 +0530

    [Access] Access provided to Sales

--- portal_access_management/models/portal_access_scheduler.py
+++ portal_access_management/models/portal_access_scheduler.py
@@ -23,9 +23,9 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
     portal_access_email_sent = fields.Boolean('Portal Access Email Sent', default=False)
 
-    signup_token = fields.Char(copy=False, groups="base.group_erp_manager,sales_team.group_sale_manager", compute='_compute_token', inverse='_inverse_token')
-    signup_type = fields.Char(string='Signup Token Type', copy=False, groups="base.group_erp_manager,sales_team.group_sale_manager")
-    signup_expiration = fields.Datetime(copy=False, groups="base.group_erp_manager,sales_team.group_sale_manager")
+    signup_token = fields.Char(copy=False, groups="base.group_erp_manager,base.group_system", compute='_compute_token', inverse='_inverse_token')
+    signup_type = fields.Char(string='Signup Token Type', copy=False, groups="base.group_erp_manager,base.group_system")
+    signup_expiration = fields.Datetime(copy=False, groups="base.group_erp_manager,base.group_system")
 
 
 class PortalAccessScheduler(models.TransientModel):
